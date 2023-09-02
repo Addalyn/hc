119c5531180ae69dac1dd55a8530511f86e80e44
zheneq <zheneq@users.noreply.github.com>
2023-09-02 11:14:31 +0400

    Update NPCBrain.cs

--- Assembly-CSharp/NPCBrain.cs
+++ Assembly-CSharp/NPCBrain.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +6,6 @@ public class NPCBrain : MonoBehaviour, IGameEventListener
 {
 	[Tooltip("Create new states in your scene, then add them to this list.")]
 	public List<StateEntry> StateTable = new List<StateEntry>();
-
 	[Tooltip("ID of the starting state for this FSM. NullStateID will use the first state in StateTable. Make sure StartingState exists in the StateTable")]
 	public StateID StartingState;
 
@@ -15,68 +13,58 @@ public class NPCBrain : MonoBehaviour, IGameEventListener
 
 	[HideInInspector]
 	public FSMSystem fsm { get; private set; }
-
 	[HideInInspector]
 	public NPCBrain NextBrain { get; internal set; }
 
 	private void Start()
 	{
-		if (base.GetComponent<BotController>() == null)
+		if (GetComponent<BotController>() == null)
 		{
-			base.name += " [Prime]";
-			base.enabled = false;
+			name += " [Prime]";
+			enabled = false;
 		}
 	}
 
 	public void OnDestroy()
 	{
 		GameEventManager.Get().RemoveAllListenersFrom(this);
-		if (this.m_allocatedStateTableParent != null)
+		if (m_allocatedStateTableParent != null)
 		{
-			UnityEngine.Object.Destroy(this.m_allocatedStateTableParent);
-			this.m_allocatedStateTableParent = null;
+			Destroy(m_allocatedStateTableParent);
+			m_allocatedStateTableParent = null;
 		}
-		if (this.fsm != null)
+		if (fsm != null)
 		{
-			this.fsm.DestroyAllStates();
+			fsm.DestroyAllStates();
 		}
 	}
 
 	public bool CanTransistion(Transition trans)
 	{
-		bool result;
-		if (this.fsm != null)
-		{
-			result = this.fsm.CanTransistion(trans);
-		}
-		else
-		{
-			result = false;
-		}
-		return result;
+		return fsm != null && fsm.CanTransistion(trans);
 	}
 
 	public void OnGameEvent(GameEventManager.EventType eventType, GameEventManager.GameEventArgs args)
 	{
-		if (this != null && this.fsm != null && base.enabled)
+		if (this != null && fsm != null && enabled)
 		{
-			this.fsm.OnGameEvent(eventType, args);
+			fsm.OnGameEvent(eventType, args);
 		}
 	}
 
 	public void SetTransition(Transition t)
 	{
-		this.fsm.PerformTransition(t, this);
+		fsm.PerformTransition(t, this);
 	}
 
 	public void SetPendingTransition(Transition t)
 	{
-		this.fsm.SetPendingTransition(t);
+		fsm.SetPendingTransition(t);
 	}
 
 	public Transition GetPendingTransition()
 	{
-		return this.fsm.GetPendingTransition();
+		return fsm.GetPendingTransition();
 	}
 
 	public virtual NPCBrain Create(BotController bot, Transform destination)
@@ -91,25 +79,24 @@ public class NPCBrain : MonoBehaviour, IGameEventListener
 
 	public virtual void SelectBotAbilityMods()
 	{
-		base.GetComponent<BotController>().SelectBotAbilityMods_Brainless();
+		GetComponent<BotController>().SelectBotAbilityMods_Brainless();
 	}
 
 	public virtual void SelectBotCards()
 	{
-		base.GetComponent<BotController>().SelectBotCards_Brainless();
+		GetComponent<BotController>().SelectBotCards_Brainless();
 	}
 
 	public IEnumerator FSMTakeTurn()
 	{
-		if (this.fsm != null)
+		if (fsm != null)
 		{
-			yield return base.StartCoroutine(this.fsm.TakeTurn());
+			yield return StartCoroutine(fsm.TakeTurn());
 		}
 		else
 		{
-			yield return base.StartCoroutine(this.DecideTurn());
+			yield return StartCoroutine(DecideTurn());
 		}
-		yield break;
 	}
 
 	protected virtual void MakeFSM(NPCBrain brainInstance)
