c6163c1e5861cbe495e8e1f6815074b8ad3651a8
zheneq <zheneq@users.noreply.github.com>
2024-04-22 16:21:42 +0400

    Update MapData.cs

--- Assembly-CSharp/MapData.cs
+++ Assembly-CSharp/MapData.cs
@@ -4,17 +4,11 @@ using System;
 public class MapData
 {
 	public string Name;
-
 	public string DisplayName;
-
 	public string ResourceImageSpriteLocation;
-
 	public string MinimapImageLocation;
-
 	public float MinimapImageXScale = 1f;
-
 	public float MinimapImageYScale = 1f;
-
 	public bool IsValidForCustomGames;
 
 	public string GetDisplayName()
