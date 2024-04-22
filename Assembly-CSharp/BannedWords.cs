957118b9cc555bee59964dd13e20c4771625fd3d
zheneq <zheneq@users.noreply.github.com>
2024-04-22 16:20:34 +0400

    Update BannedWords.cs

--- Assembly-CSharp/BannedWords.cs
+++ Assembly-CSharp/BannedWords.cs
@@ -4,120 +4,92 @@ using System.Text;
 [Serializable]
 public class BannedWords
 {
-	public BannedWordsData[] m_bannedWords;
+    public BannedWordsData[] m_bannedWords;
 
-	public static BannedWords Get()
-	{
-		return GameWideData.Get().m_bannedWords;
-	}
+    public static BannedWords Get()
+    {
+        return GameWideData.Get().m_bannedWords;
+    }
 
-	public string FilterPhrase(string phrase, string languageCode)
-	{
-		if (phrase.IsNullOrEmpty())
-		{
-			while (true)
-			{
-				switch (7)
-				{
-				case 0:
-					break;
-				default:
-					return phrase;
-				}
-			}
-		}
-		StringBuilder stringBuilder = new StringBuilder(phrase);
-		char[] anyOf = " ,./?'<>:;\"[]{}-_+=!@#^&*()\\|".ToCharArray();
-		int num = 0;
-		while (true)
-		{
-			if (num < phrase.Length)
-			{
-				int num2 = phrase.IndexOfAny(anyOf, num);
-				int num3;
-				if (num2 < 0)
-				{
-					num3 = phrase.Length - num;
-				}
-				else
-				{
-					num3 = num2 - num;
-				}
-				int num4 = num3;
-				if (num4 > 0)
-				{
-					string text = phrase.Substring(num, num4).ToLower();
-					BannedWordsData[] bannedWords = m_bannedWords;
-					foreach (BannedWordsData bannedWordsData in bannedWords)
-					{
-						if (!(bannedWordsData.Name == languageCode))
-						{
-							continue;
-						}
-						string[] fullStrings = bannedWordsData.m_fullStrings;
-						foreach (string b in fullStrings)
-						{
-							if (text == b)
-							{
-								Mask(stringBuilder, num, num4);
-							}
-						}
-						string[] subStrings = bannedWordsData.m_subStrings;
-						foreach (string text2 in subStrings)
-						{
-							int length = text2.Length;
-							int num5 = 0;
-							while (true)
-							{
-								if (num5 < num4)
-								{
-									num5 = text.IndexOf(text2, num5);
-									if (num5 == -1)
-									{
-										break;
-									}
-									Mask(stringBuilder, num + num5, length);
-									num5++;
-									continue;
-								}
-								break;
-							}
-						}
-						string[] prefixStrings = bannedWordsData.m_prefixStrings;
-						foreach (string text3 in prefixStrings)
-						{
-							int length2 = text3.Length;
-							if (text.StartsWith(text3))
-							{
-								Mask(stringBuilder, num, length2);
-							}
-						}
-						string[] suffixStrings = bannedWordsData.m_suffixStrings;
-						foreach (string text4 in suffixStrings)
-						{
-							int length3 = text4.Length;
-							if (text.EndsWith(text4))
-							{
-								Mask(stringBuilder, num + num4 - length3, length3);
-							}
-						}
-					}
-				}
-				if (num2 < 0)
-				{
-					break;
-				}
-				num = num2 + 1;
-				continue;
-			}
-			break;
-		}
-		return stringBuilder.ToString();
-	}
+    public string FilterPhrase(string phrase, string languageCode)
+    {
+        if (phrase.IsNullOrEmpty())
+        {
+            return phrase;
+        }
 
-	private void Mask(StringBuilder stringBuilder, int maskStartIndex, int maskLength)
-	{
-		stringBuilder.Remove(maskStartIndex, maskLength);
-		stringBuilder.Insert(maskStartIndex, new string('*', maskLength));
-	}
+        StringBuilder stringBuilder = new StringBuilder(phrase);
+        char[] anyOf = " ,./?'<>:;\"[]{}-_+=!@#^&*()\\|".ToCharArray();
+        int startIndex = 0;
+        while (startIndex < phrase.Length)
+        {
+            int endIndex = phrase.IndexOfAny(anyOf, startIndex);
+            int len = endIndex < 0 ? phrase.Length - startIndex : endIndex - startIndex;
+            if (len > 0)
+            {
+                string text = phrase.Substring(startIndex, len).ToLower();
+                BannedWordsData[] bannedWords = m_bannedWords;
+                foreach (BannedWordsData bannedWordsData in bannedWords)
+                {
+                    if (bannedWordsData.Name != languageCode)
+                    {
+                        continue;
+                    }
+
+                    foreach (string bannedFullString in bannedWordsData.m_fullStrings)
+                    {
+                        if (text == bannedFullString)
+                        {
+                            Mask(stringBuilder, startIndex, len);
+                        }
+                    }
+
+                    foreach (string bannedSubString in bannedWordsData.m_subStrings)
+                    {
+                        for (int i = 0; i < len; i++)
+                        {
+                            i = text.IndexOf(bannedSubString, i);
+                            if (i == -1)
+                            {
+                                break;
+                            }
+
+                            Mask(stringBuilder, startIndex + i, bannedSubString.Length);
+                        }
+                    }
+
+                    foreach (string bannedPrefix in bannedWordsData.m_prefixStrings)
+                    {
+                        if (text.StartsWith(bannedPrefix))
+                        {
+                            Mask(stringBuilder, startIndex, bannedPrefix.Length);
+                        }
+                    }
+
+                    foreach (string bannedSuffix in bannedWordsData.m_suffixStrings)
+                    {
+                        if (text.EndsWith(bannedSuffix))
+                        {
+                            Mask(stringBuilder, startIndex + len - bannedSuffix.Length, bannedSuffix.Length);
+                        }
+                    }
+                }
+            }
+
+            if (endIndex < 0)
+            {
+                break;
+            }
+
+            startIndex = endIndex + 1;
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private void Mask(StringBuilder stringBuilder, int maskStartIndex, int maskLength)
+    {
+        stringBuilder.Remove(maskStartIndex, maskLength);
+        stringBuilder.Insert(maskStartIndex, new string('*', maskLength));
+    }
 }
