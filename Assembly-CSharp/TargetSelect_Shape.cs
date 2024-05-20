2daa0279f69da4913a5319ef260ae2b733b07492
zheneq <zheneq@users.noreply.github.com>
2024-05-21 03:56:16 +0400

    Rogues comments TargetSelect_Shape.cs

--- Assembly-CSharp/TargetSelect_Shape.cs
+++ Assembly-CSharp/TargetSelect_Shape.cs
@@ -1,3 +1,5 @@
+// ROGUES
+// SERVER
 using System.Collections.Generic;
 using AbilityContextNamespace;
 using UnityEngine;
@@ -7,7 +9,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
     public delegate BoardSquare CenterSquareDelegate(AbilityTarget currentTarget, ActorData caster);
     public delegate bool IsMovingShapeDelegate(ActorData caster);
     public delegate BoardSquare GetMoveStartSquareDelegate(AbilityTarget currentTarget, ActorData caster);
-    public delegate Vector3 GetMoveStartFreePosDelegate(AbilityTarget currentTarget, ActorData caster);
+    public delegate Vector3 GetMoveStartFreePosDelegate(AbilityTarget currentTarget, ActorData caster); // removed in rogues
 
     [Separator("Targeting Properties")] public AbilityAreaShape m_shape = AbilityAreaShape.Three_x_Three;
     public List<AbilityAreaShape> m_additionalShapes = new List<AbilityAreaShape>();
@@ -16,21 +18,46 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
     public bool m_canTargetOnEnemies = true;
     public bool m_canTargetOnAllies = true;
     public bool m_canTargetOnSelf = true;
-    public bool m_ignoreLosToTargetActor;
+    public bool m_ignoreLosToTargetActor; // TODO ICEBORG removed in rogues
     [Separator("Show targeter arc?")] public bool m_showTargeterArc;
     [Separator("Use Move Shape Targeter? (for moving a shape similar to Grey drone)")]
     public bool m_useMoveShapeTargeter;
     public float m_moveLineWidth = 1f;
     [Separator("Sequences")] public GameObject m_castSequencePrefab;
 
+    // public string m_centerPosContextKeyName = ContextKeys.s_CenterPos.GetName(); // rogues
     public CenterSquareDelegate m_centerSquareDelegate;
     public IsMovingShapeDelegate m_isMovingShapeDelegate;
     public GetMoveStartSquareDelegate m_moveStartSquareDelegate;
-    public GetMoveStartFreePosDelegate m_moveStartFreePosDelegate;
+    public GetMoveStartFreePosDelegate m_moveStartFreePosDelegate; // TODO ICEBORG removed in rogues
 
     private const string c_shapeLayer = "ShapeLayer";
     public static ContextNameKeyPair s_cvarShapeLayer = new ContextNameKeyPair("ShapeLayer");
+    // public static ContextNameKeyPair s_cvarShapeSideWidth = new ContextNameKeyPair("ShapeSideWidth"); // rogues
+    //
+    // public static ContextNameKeyPair[] s_cvarShapeSideCenters = { // rogues
+    // 	new ContextNameKeyPair("ShapeSide1Center"),
+    // 	new ContextNameKeyPair("ShapeSide2Center"),
+    // 	new ContextNameKeyPair("ShapeSide3Center"),
+    // 	new ContextNameKeyPair("ShapeSide4Center"),
+    // 	new ContextNameKeyPair("ShapeSide5Center"),
+    // 	new ContextNameKeyPair("ShapeSide6Center"),
+    // 	new ContextNameKeyPair("ShapeSide7Center"),
+    // 	new ContextNameKeyPair("ShapeSide8Center")
+    // };
+    //
+    // public static ContextNameKeyPair[] s_cvarShapeSideFacingDirs = { // rogues
+    // 	new ContextNameKeyPair("ShapeSide1Facing"),
+    // 	new ContextNameKeyPair("ShapeSide2Facing"),
+    // 	new ContextNameKeyPair("ShapeSide3Facing"),
+    // 	new ContextNameKeyPair("ShapeSide4Facing"),
+    // 	new ContextNameKeyPair("ShapeSide5Facing"),
+    // 	new ContextNameKeyPair("ShapeSide6Facing"),
+    // 	new ContextNameKeyPair("ShapeSide7Facing"),
+    // 	new ContextNameKeyPair("ShapeSide8Facing")
+    // };
 
+    // private ContextNameKeyPair m_centerPosContextKey; // rogues
     private TargetSelectMod_Shape m_targetSelMod;
     private List<AbilityAreaShape> m_shapesList = new List<AbilityAreaShape>();
 
@@ -40,15 +67,18 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
             "ShapeLayer",
             "on every hit actor, smallest shape index that actor is hit in (0-based). " +
             "Shapes are sorted from smallest to largest");
+        // + GetContextUsageStr(m_centerPosContextKeyName, "non-actor specific context, center position of shape"); // rogues
     }
 
     public override void ListContextNamesForEditor(List<string> names)
     {
         names.Add("ShapeLayer");
+        // names.Add(m_centerPosContextKeyName); // rogues
     }
 
     public override void Initialize()
     {
+        // m_centerPosContextKey = new ContextNameKeyPair(m_centerPosContextKeyName); // rogues
         InitShapesList();
     }
 
@@ -63,6 +93,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
         m_shapesList.Sort();
     }
 
+    // removed in rogues
     public bool RequireTargetingOnActor()
     {
         return m_targetSelMod != null
@@ -70,6 +101,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
             : m_requireTargetingOnActor;
     }
 
+    // removed in rogues
     public bool CanTargetOnEnemies()
     {
         return m_targetSelMod != null
@@ -77,6 +109,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
             : m_canTargetOnEnemies;
     }
 
+    // removed in rogues
     public bool CanTargetOnAllies()
     {
         return m_targetSelMod != null
@@ -84,6 +117,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
             : m_canTargetOnAllies;
     }
 
+    // removed in rogues
     public bool CanTargetOnSelf()
     {
         return m_targetSelMod != null
@@ -91,6 +125,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
             : m_canTargetOnSelf;
     }
 
+    // removed in rogues
     public bool IgnoreLosToTargetActor()
     {
         return m_targetSelMod != null
@@ -98,6 +133,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
             : m_ignoreLosToTargetActor;
     }
 
+    // removed in rogues
     public float GetMoveLineWidth()
     {
         return m_targetSelMod != null
@@ -118,7 +154,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
                 IncludeAllies(),
                 IncludeCaster());
             targeter.SetAffectedGroups(IncludeEnemies(), IncludeAllies(), IncludeCaster());
-            targeter.m_alwaysIncludeShapeCenterActor = RequireTargetingOnActor();
+            targeter.m_alwaysIncludeShapeCenterActor = RequireTargetingOnActor(); // m_requireTargetingOnActor in rogues
             targeter.SetShowArcToShape(m_showTargeterArc);
             return new List<AbilityUtil_Targeter> { targeter };
         }
@@ -128,12 +164,14 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
                 ability,
                 GetShape(),
                 IgnoreLos(),
-                GetMoveLineWidth());
+                GetMoveLineWidth()); // m_moveLineWidth in rogues
             targeter.SetAffectedGroups(IncludeEnemies(), IncludeAllies(), IncludeCaster());
+            // removed in rogues
             if (!IncludeAllies() && !IncludeCaster())
             {
                 targeter.m_affectsCaster = AbilityUtil_Targeter.AffectsActor.Never;
             }
+            // end removed in rogues
 
             targeter.SetShowArcToShape(m_showTargeterArc);
             return new List<AbilityUtil_Targeter> { targeter };
@@ -142,20 +180,20 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
 
     public override bool HandleCanCastValidation(Ability ability, ActorData caster)
     {
-        if (!RequireTargetingOnActor())
+        if (!RequireTargetingOnActor()) // m_requireTargetingOnActor in rogues
         {
             return true;
         }
 
         return ability.HasTargetableActorsInDecision(
             caster,
-            CanTargetOnEnemies(),
-            CanTargetOnAllies(),
-            CanTargetOnSelf(),
+            CanTargetOnEnemies(), // m_canTargetOnEnemies in rogues
+            CanTargetOnAllies(), // m_canTargetOnAllies in rogues
+            CanTargetOnSelf(), // m_canTargetOnSelf in rogues
             Ability.ValidateCheckPath.Ignore,
-            !IgnoreLosToTargetActor(),
+            !IgnoreLosToTargetActor(), // true in rogues
             false,
-            true);
+            true); // false in rogues
     }
 
     public override bool HandleCustomTargetValidation(
@@ -165,7 +203,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
         int targetIndex,
         List<AbilityTarget> currentTargets)
     {
-        if (!RequireTargetingOnActor())
+        if (!RequireTargetingOnActor()) // m_requireTargetingOnActor in rogues
         {
             return true;
         }
@@ -178,13 +216,13 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
                && ability.CanTargetActorInDecision(
                    caster,
                    targetActor,
-                   CanTargetOnEnemies(),
-                   CanTargetOnAllies(),
-                   CanTargetOnSelf(),
+                   CanTargetOnEnemies(), // m_canTargetOnEnemies in rogues
+                   CanTargetOnAllies(), // m_canTargetOnAllies in rogues
+                   CanTargetOnSelf(), // m_canTargetOnSelf in rogues
                    Ability.ValidateCheckPath.Ignore,
-                   !IgnoreLosToTargetActor(),
+                   !IgnoreLosToTargetActor(), // true in rogues
                    false,
-                   true);
+                   true); // false in rogues
     }
 
     protected override void OnTargetSelModApplied(TargetSelectModBase modBase)
@@ -223,6 +261,7 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
             : caster.GetCurrentBoardSquare();
     }
 
+    // removed in rogues
     public Vector3 GetMoveStartFreePos(AbilityTarget target, ActorData caster)
     {
         return m_moveStartFreePosDelegate != null
@@ -264,18 +303,18 @@ public class TargetSelect_Shape : GenericAbility_TargetSelectBase
         BoardSquare shapeCenterSquare = GetShapeCenterSquare(targets[0], caster);
         Vector3 centerOfShape = AreaEffectUtils.GetCenterOfShape(GetShape(), targets[0].FreePos, shapeCenterSquare);
         ContextVars nonActorSpecificContext = GetNonActorSpecificContext();
-        // nonActorSpecificContext.SetValue(this.m_centerPosContextKey.GetKey(), centerOfShape);
+        // nonActorSpecificContext.SetValue(m_centerPosContextKey.GetKey(), centerOfShape);
         List<BarrierPoseInfo> barrierPosesForRegularPolygon = BarrierPoseInfo.GetBarrierPosesForRegularPolygon(
             centerOfShape,
             AreaEffectUtils.GetNumberOfSidesForShape(GetShape()),
             AreaEffectUtils.GetWidthForShape(GetShape()) * 0.5f * Board.SquareSizeStatic);
-        //if (!barrierPosesForRegularPolygon.IsNullOrEmpty<BarrierPoseInfo>())
+        //if (!barrierPosesForRegularPolygon.IsNullOrEmpty())
         //{
-        //    nonActorSpecificContext.SetValue(TargetSelect_Shape.s_cvarShapeSideWidth.GetKey(), barrierPosesForRegularPolygon[0].widthInWorld);
+        //    nonActorSpecificContext.SetValue(s_cvarShapeSideWidth.GetKey(), barrierPosesForRegularPolygon[0].widthInWorld);
         //    for (int i = 0; i < barrierPosesForRegularPolygon.Count; i++)
         //    {
-        //        nonActorSpecificContext.SetValue(TargetSelect_Shape.s_cvarShapeSideCenters[i].GetKey(), barrierPosesForRegularPolygon[i].midpoint);
-        //        nonActorSpecificContext.SetValue(TargetSelect_Shape.s_cvarShapeSideFacingDirs[i].GetKey(), barrierPosesForRegularPolygon[i].facingDirection);
+        //        nonActorSpecificContext.SetValue(s_cvarShapeSideCenters[i].GetKey(), barrierPosesForRegularPolygon[i].midpoint);
+        //        nonActorSpecificContext.SetValue(s_cvarShapeSideFacingDirs[i].GetKey(), barrierPosesForRegularPolygon[i].facingDirection);
         //    }
         //}
         for (int i = 0; i < hitActors.Count; i++)
