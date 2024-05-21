82c94b18d6e54f6f1af4827676ffdc15c19d7867
zheneq <zheneq@users.noreply.github.com>
2024-05-21 16:46:21 +0400

    Update TargetSelect_LaserTargetedPull.cs

--- Assembly-CSharp/TargetSelect_LaserTargetedPull.cs
+++ Assembly-CSharp/TargetSelect_LaserTargetedPull.cs
@@ -4,189 +4,202 @@ using UnityEngine.Networking;
 
 public class TargetSelect_LaserTargetedPull : GenericAbility_TargetSelectBase
 {
-	[Separator("Targeting Properties", true)]
-	public float m_laserRange = 5f;
+    [Separator("Targeting Properties")]
+    public float m_laserRange = 5f;
+    public float m_laserWidth = 1f;
+    public int m_maxTargets = 1;
+    public float m_maxKnockbackDist = 50f;
+    [Separator("For Pull Destination")]
+    public bool m_casterSquareValidForKnockback = true;
+    public float m_squareRangeFromCaster = 3f;
+    public float m_destinationAngleDegWithBack = 360f;
+    public bool m_destRequireLosFromCaster = true;
+    [Separator("Sequences")]
+    public GameObject m_castSequencePrefab;
 
-	public float m_laserWidth = 1f;
+    private TargetSelectMod_LaserTargetedPull m_targetSelMod;
 
-	public int m_maxTargets = 1;
+    public override string GetUsageForEditor()
+    {
+        return string.Empty;
+    }
 
-	public float m_maxKnockbackDist = 50f;
+    public override List<AbilityUtil_Targeter> CreateTargeters(Ability ability)
+    {
+        
+        AbilityUtil_Targeter_Laser targeterLaser = new AbilityUtil_Targeter_Laser(
+            ability,
+            GetLaserWidth(),
+            GetLaserRange(),
+            false,
+            GetMaxTargets(),
+            IncludeAllies(),
+            IncludeCaster());
+        targeterLaser.SetUseMultiTargetUpdate(true);
+        
+        AbilityUtil_Targeter_RampartGrab targeterGrab = new AbilityUtil_Targeter_RampartGrab(
+            ability,
+            AbilityAreaShape.SingleSquare,
+            GetMaxKnockbackDist(),
+            KnockbackType.PullToSource,
+            GetLaserRange(),
+            m_laserWidth,
+            false,
+            GetMaxTargets());
+        targeterGrab.SetUseMultiTargetUpdate(true);
 
-	[Separator("For Pull Destination", true)]
-	public bool m_casterSquareValidForKnockback = true;
+        return new List<AbilityUtil_Targeter>
+        {
+            targeterLaser,
+            targeterGrab
+        };
+    }
 
-	public float m_squareRangeFromCaster = 3f;
+    protected override void OnTargetSelModApplied(TargetSelectModBase modBase)
+    {
+        m_targetSelMod = modBase as TargetSelectMod_LaserTargetedPull;
+    }
 
-	public float m_destinationAngleDegWithBack = 360f;
+    protected override void OnTargetSelModRemoved()
+    {
+        m_targetSelMod = null;
+    }
 
-	public bool m_destRequireLosFromCaster = true;
+    public float GetLaserRange()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_laserRangeMod.GetModifiedValue(m_laserRange)
+            : m_laserRange;
+    }
 
-	[Separator("Sequences", true)]
-	public GameObject m_castSequencePrefab;
+    public float GetLaserWidth()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_laserWidthMod.GetModifiedValue(m_laserWidth)
+            : m_laserWidth;
+    }
 
-	private TargetSelectMod_LaserTargetedPull m_targetSelMod;
+    public int GetMaxTargets()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_maxTargetsMod.GetModifiedValue(m_maxTargets)
+            : m_maxTargets;
+    }
 
-	public override string GetUsageForEditor()
-	{
-		return string.Empty;
-	}
+    public float GetMaxKnockbackDist()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_maxKnockbackDistMod.GetModifiedValue(m_maxKnockbackDist)
+            : m_maxKnockbackDist;
+    }
 
-	public override List<AbilityUtil_Targeter> CreateTargeters(Ability ability)
-	{
-		List<AbilityUtil_Targeter> list = new List<AbilityUtil_Targeter>();
-		AbilityUtil_Targeter_Laser abilityUtil_Targeter_Laser = new AbilityUtil_Targeter_Laser(ability, GetLaserWidth(), GetLaserRange(), false, GetMaxTargets(), IncludeAllies(), IncludeCaster());
-		abilityUtil_Targeter_Laser.SetUseMultiTargetUpdate(true);
-		list.Add(abilityUtil_Targeter_Laser);
-		AbilityUtil_Targeter_RampartGrab abilityUtil_Targeter_RampartGrab = new AbilityUtil_Targeter_RampartGrab(ability, AbilityAreaShape.SingleSquare, GetMaxKnockbackDist(), KnockbackType.PullToSource, GetLaserRange(), m_laserWidth, false, GetMaxTargets());
-		abilityUtil_Targeter_RampartGrab.SetUseMultiTargetUpdate(true);
-		list.Add(abilityUtil_Targeter_RampartGrab);
-		return list;
-	}
+    public float GetSquareRangeFromCaster()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_squareRangeFromCasterMod.GetModifiedValue(m_squareRangeFromCaster)
+            : m_squareRangeFromCaster;
+    }
 
-	protected override void OnTargetSelModApplied(TargetSelectModBase modBase)
-	{
-		m_targetSelMod = (modBase as TargetSelectMod_LaserTargetedPull);
-	}
+    public float GetDestinationAngleDegWithBack()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_destinationAngleDegWithBackMod.GetModifiedValue(m_destinationAngleDegWithBack)
+            : m_destinationAngleDegWithBack;
+    }
 
-	protected override void OnTargetSelModRemoved()
-	{
-		m_targetSelMod = null;
-	}
+    public override bool HandleCustomTargetValidation(
+        Ability ability,
+        ActorData caster,
+        AbilityTarget target,
+        int targetIndex,
+        List<AbilityTarget> currentTargets)
+    {
+        if (targetIndex > 0)
+        {
+            BoardSquare boardSquareSafe = Board.Get().GetSquare(target.GridPos);
+            BoardSquare currentBoardSquare = caster.GetCurrentBoardSquare();
+            if (boardSquareSafe == currentBoardSquare)
+            {
+                return m_casterSquareValidForKnockback;
+            }
 
-	public float GetLaserRange()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_laserRangeMod.GetModifiedValue(m_laserRange);
-		}
-		else
-		{
-			result = m_laserRange;
-		}
-		return result;
-	}
+            if (GetSquareRangeFromCaster() > 0f
+                && currentBoardSquare.HorizontalDistanceInSquaresTo(boardSquareSafe) > GetSquareRangeFromCaster())
+            {
+                return false;
+            }
 
-	public float GetLaserWidth()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_laserWidthMod.GetModifiedValue(m_laserWidth);
-		}
-		else
-		{
-			result = m_laserWidth;
-		}
-		return result;
-	}
+            if (m_destRequireLosFromCaster
+                && !currentBoardSquare.GetLOS(boardSquareSafe.x, boardSquareSafe.y))
+            {
+                return false;
+            }
 
-	public int GetMaxTargets()
-	{
-		return (m_targetSelMod == null) ? m_maxTargets : m_targetSelMod.m_maxTargetsMod.GetModifiedValue(m_maxTargets);
-	}
+            Vector3 from = -1f * currentTargets[0].AimDirection;
+            Vector3 to = boardSquareSafe.ToVector3() - caster.GetFreePos();
+            from.y = 0f;
+            to.y = 0f;
+            int num = Mathf.RoundToInt(Vector3.Angle(from, to));
+            if (num > GetDestinationAngleDegWithBack())
+            {
+                return false;
+            }
 
-	public float GetMaxKnockbackDist()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_maxKnockbackDistMod.GetModifiedValue(m_maxKnockbackDist);
-		}
-		else
-		{
-			result = m_maxKnockbackDist;
-		}
-		return result;
-	}
+            if (NetworkClient.active)
+            {
+                List<ActorData> visibleActorsInRangeByTooltipSubject = ability.Targeters[0]
+                    .GetVisibleActorsInRangeByTooltipSubject(AbilityTooltipSubject.Enemy);
+                if (visibleActorsInRangeByTooltipSubject.Count > 0)
+                {
+                    bool flag = false;
+                    foreach (var actorData in visibleActorsInRangeByTooltipSubject)
+                    {
+                        if (flag)
+                        {
+                            break;
+                        }
 
-	public float GetSquareRangeFromCaster()
-	{
-		return (m_targetSelMod == null) ? m_squareRangeFromCaster : m_targetSelMod.m_squareRangeFromCasterMod.GetModifiedValue(m_squareRangeFromCaster);
-	}
+                        BoardSquare actorSquare = actorData.GetCurrentBoardSquare();
+                        flag = KnockbackUtils.CanBuildStraightLineChargePath(
+                            caster,
+                            boardSquareSafe,
+                            actorSquare,
+                            false,
+                            out int _);
+                    }
 
-	public float GetDestinationAngleDegWithBack()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_destinationAngleDegWithBackMod.GetModifiedValue(m_destinationAngleDegWithBack);
-		}
-		else
-		{
-			result = m_destinationAngleDegWithBack;
-		}
-		return result;
-	}
+                    if (!flag)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
 
-	public override bool HandleCustomTargetValidation(Ability ability, ActorData caster, AbilityTarget target, int targetIndex, List<AbilityTarget> currentTargets)
-	{
-		if (targetIndex > 0)
-		{
-			BoardSquare boardSquareSafe = Board.Get().GetSquare(target.GridPos);
-			BoardSquare currentBoardSquare = caster.GetCurrentBoardSquare();
-			if (boardSquareSafe == currentBoardSquare)
-			{
-				while (true)
-				{
-					return m_casterSquareValidForKnockback;
-				}
-			}
-			if (GetSquareRangeFromCaster() > 0f)
-			{
-				if (currentBoardSquare.HorizontalDistanceInSquaresTo(boardSquareSafe) > GetSquareRangeFromCaster())
-				{
-					return false;
-				}
-			}
-			if (m_destRequireLosFromCaster)
-			{
-				if (!currentBoardSquare.GetLOS(boardSquareSafe.x, boardSquareSafe.y))
-				{
-					return false;
-				}
-			}
-			Vector3 from = -1f * currentTargets[0].AimDirection;
-			Vector3 to = boardSquareSafe.ToVector3() - caster.GetFreePos();
-			from.y = 0f;
-			to.y = 0f;
-			int num = Mathf.RoundToInt(Vector3.Angle(from, to));
-			if ((float)num > GetDestinationAngleDegWithBack())
-			{
-				return false;
-			}
-			if (NetworkClient.active)
-			{
-				List<ActorData> visibleActorsInRangeByTooltipSubject = ability.Targeters[0].GetVisibleActorsInRangeByTooltipSubject(AbilityTooltipSubject.Enemy);
-				if (visibleActorsInRangeByTooltipSubject.Count > 0)
-				{
-					bool flag = false;
-					for (int i = 0; i < visibleActorsInRangeByTooltipSubject.Count; i++)
-					{
-						if (flag)
-						{
-							break;
-						}
-						BoardSquare currentBoardSquare2 = visibleActorsInRangeByTooltipSubject[i].GetCurrentBoardSquare();
-						flag = KnockbackUtils.CanBuildStraightLineChargePath(caster, boardSquareSafe, currentBoardSquare2, false, out int _);
-					}
-					if (!flag)
-					{
-						return false;
-					}
-				}
-			}
-		}
-		return base.HandleCustomTargetValidation(ability, caster, target, targetIndex, currentTargets);
-	}
+        return base.HandleCustomTargetValidation(ability, caster, target, targetIndex, currentTargets);
+    }
 
-	private List<ActorData> GetHitActors(List<AbilityTarget> targets, ActorData caster, out List<Vector3> targetPosForSequences, List<NonActorTargetInfo> nonActorTargetInfo)
-	{
-		targetPosForSequences = new List<Vector3>();
-		Vector3 laserEndPos;
-		List<ActorData> actorsInLaser = AreaEffectUtils.GetActorsInLaser(caster.GetLoSCheckPos(), targets[0].AimDirection, GetLaserRange(), GetLaserWidth(), caster, TargeterUtils.GetRelevantTeams(caster, IncludeAllies(), IncludeEnemies()), false, GetMaxTargets(), false, true, out laserEndPos, nonActorTargetInfo);
-		targetPosForSequences.Add(laserEndPos);
-		return actorsInLaser;
-	}
+    private List<ActorData> GetHitActors(
+        List<AbilityTarget> targets,
+        ActorData caster,
+        out List<Vector3> targetPosForSequences,
+        List<NonActorTargetInfo> nonActorTargetInfo)
+    {
+        targetPosForSequences = new List<Vector3>();
+        List<ActorData> actorsInLaser = AreaEffectUtils.GetActorsInLaser(
+            caster.GetLoSCheckPos(),
+            targets[0].AimDirection,
+            GetLaserRange(),
+            GetLaserWidth(),
+            caster,
+            TargeterUtils.GetRelevantTeams(caster, IncludeAllies(), IncludeEnemies()),
+            false,
+            GetMaxTargets(),
+            false,
+            true,
+            out Vector3 laserEndPos,
+            nonActorTargetInfo);
+        targetPosForSequences.Add(laserEndPos);
+        return actorsInLaser;
+    }
 }
