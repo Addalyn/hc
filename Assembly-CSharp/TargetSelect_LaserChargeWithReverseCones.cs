ebf37e26b3a8274c596562727a0e77537224c19a
zheneq <zheneq@users.noreply.github.com>
2024-05-27 08:53:50 +0400

    Update TargetSelect_LaserChargeWithReverseCones.cs

--- Assembly-CSharp/TargetSelect_LaserChargeWithReverseCones.cs
+++ Assembly-CSharp/TargetSelect_LaserChargeWithReverseCones.cs
@@ -1,238 +1,193 @@
-using AbilityContextNamespace;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using AbilityContextNamespace;
 using UnityEngine;
 
 public class TargetSelect_LaserChargeWithReverseCones : GenericAbility_TargetSelectBase
 {
-	[Separator("Targeting Properties", true)]
-	public float m_laserRange = 5f;
+    [Separator("Targeting Properties")]
+    public float m_laserRange = 5f;
+    public float m_laserWidth = 1f;
+    [Header("Cone Properties")]
+    public ConeTargetingInfo m_coneInfo;
+    [Space(10f)]
+    public int m_coneCount = 3;
+    public float m_coneStartOffset;
+    public float m_perConeHorizontalOffset;
+    public float m_angleInBetween = 10f;
+    [Separator("Sequences")]
+    public GameObject m_castSequencePrefab;
+    public GameObject m_coneSequencePrefab;
 
-	public float m_laserWidth = 1f;
+    private const string c_directChargeHit = "DirectChargeHit";
+    public static ContextNameKeyPair s_cvarDirectChargeHit = new ContextNameKeyPair(c_directChargeHit);
 
-	[Header("Cone Properties")]
-	public ConeTargetingInfo m_coneInfo;
+    private TargetSelectMod_LaserChargeWithReverseCones m_targetSelMod;
+    private ConeTargetingInfo m_cachedConeInfo;
 
-	[Space(10f)]
-	public int m_coneCount = 3;
+    public override string GetUsageForEditor()
+    {
+        return GetContextUsageStr(
+            s_cvarDirectChargeHit.GetName(),
+            "whether this is a direct charge hit or not (if not, it's a cone hit)");
+    }
 
-	public float m_coneStartOffset;
+    public override void ListContextNamesForEditor(List<string> names)
+    {
+        names.Add(c_directChargeHit);
+    }
 
-	public float m_perConeHorizontalOffset;
+    public override void Initialize()
+    {
+        SetCachedFields();
+        ConeTargetingInfo coneInfo = GetConeInfo();
+        coneInfo.m_affectsAllies = IncludeAllies();
+        coneInfo.m_affectsEnemies = IncludeEnemies();
+        coneInfo.m_affectsCaster = IncludeCaster();
+        coneInfo.m_penetrateLos = IgnoreLos();
+    }
 
-	public float m_angleInBetween = 10f;
+    public override List<AbilityUtil_Targeter> CreateTargeters(Ability ability)
+    {
+        return new List<AbilityUtil_Targeter>
+        {
+            new AbilityUtil_Targeter_LaserChargeReverseCones(
+                ability,
+                GetLaserWidth(),
+                GetLaserRange(),
+                GetConeInfo(),
+                GetConeCount(),
+                GetConeStartOffset(),
+                GetPerConeHorizontalOffset(),
+                GetAngleInBetween(),
+                GetConeOrigins,
+                GetConeDirections)
+            {
+                m_coneLosCheckDelegate = CustomLosForCone
+            }
+        };
+    }
 
-	[Separator("Sequences", true)]
-	public GameObject m_castSequencePrefab;
+    protected override void OnTargetSelModApplied(TargetSelectModBase modBase)
+    {
+        m_targetSelMod = modBase as TargetSelectMod_LaserChargeWithReverseCones;
+    }
 
-	public GameObject m_coneSequencePrefab;
+    protected override void OnTargetSelModRemoved()
+    {
+        m_targetSelMod = null;
+    }
 
-	private const string c_directChargeHit = "DirectChargeHit";
+    private void SetCachedFields()
+    {
+        m_cachedConeInfo = m_targetSelMod != null
+            ? m_targetSelMod.m_coneInfoMod.GetModifiedValue(m_coneInfo)
+            : m_coneInfo;
+    }
 
-	public static ContextNameKeyPair s_cvarDirectChargeHit = new ContextNameKeyPair("DirectChargeHit");
+    public float GetLaserRange()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_laserRangeMod.GetModifiedValue(m_laserRange)
+            : m_laserRange;
+    }
 
-	private TargetSelectMod_LaserChargeWithReverseCones m_targetSelMod;
+    public float GetLaserWidth()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_laserWidthMod.GetModifiedValue(m_laserWidth)
+            : m_laserWidth;
+    }
 
-	private ConeTargetingInfo m_cachedConeInfo;
+    public ConeTargetingInfo GetConeInfo()
+    {
+        return m_cachedConeInfo ?? m_coneInfo;
+    }
 
-	[CompilerGenerated]
-	private static AbilityUtil_Targeter_LaserChargeReverseCones.ConeLosCheckerDelegate _003C_003Ef__mg_0024cache0;
+    public int GetConeCount()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_coneCountMod.GetModifiedValue(m_coneCount)
+            : m_coneCount;
+    }
 
-	public override string GetUsageForEditor()
-	{
-		return GetContextUsageStr(s_cvarDirectChargeHit.GetName(), "whether this is a direct charge hit or not (if not, it's a cone hit)");
-	}
+    public float GetConeStartOffset()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_coneStartOffsetMod.GetModifiedValue(m_coneStartOffset)
+            : m_coneStartOffset;
+    }
 
-	public override void ListContextNamesForEditor(List<string> names)
-	{
-		names.Add("DirectChargeHit");
-	}
+    public float GetPerConeHorizontalOffset()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_perConeHorizontalOffsetMod.GetModifiedValue(m_perConeHorizontalOffset)
+            : m_perConeHorizontalOffset;
+    }
 
-	public override void Initialize()
-	{
-		SetCachedFields();
-		ConeTargetingInfo coneInfo = GetConeInfo();
-		coneInfo.m_affectsAllies = IncludeAllies();
-		coneInfo.m_affectsEnemies = IncludeEnemies();
-		coneInfo.m_affectsCaster = IncludeCaster();
-		coneInfo.m_penetrateLos = IgnoreLos();
-	}
+    public float GetAngleInBetween()
+    {
+        return m_targetSelMod != null
+            ? m_targetSelMod.m_angleInBetweenMod.GetModifiedValue(m_angleInBetween)
+            : m_angleInBetween;
+    }
 
-	public override List<AbilityUtil_Targeter> CreateTargeters(Ability ability)
-	{
-		List<AbilityUtil_Targeter> list = new List<AbilityUtil_Targeter>();
-		AbilityUtil_Targeter_LaserChargeReverseCones abilityUtil_Targeter_LaserChargeReverseCones = new AbilityUtil_Targeter_LaserChargeReverseCones(ability, GetLaserWidth(), GetLaserRange(), GetConeInfo(), GetConeCount(), GetConeStartOffset(), GetPerConeHorizontalOffset(), GetAngleInBetween(), GetConeOrigins, GetConeDirections);
-		abilityUtil_Targeter_LaserChargeReverseCones.m_coneLosCheckDelegate = CustomLosForCone;
-		list.Add(abilityUtil_Targeter_LaserChargeReverseCones);
-		return list;
-	}
+    public override ActorData.MovementType GetMovementType()
+    {
+        return ActorData.MovementType.Charge;
+    }
 
-	protected override void OnTargetSelModApplied(TargetSelectModBase modBase)
-	{
-		m_targetSelMod = (modBase as TargetSelectMod_LaserChargeWithReverseCones);
-	}
+    protected List<Vector3> GetConeOrigins(AbilityTarget currentTarget, Vector3 targeterFreePos, ActorData caster)
+    {
+        List<Vector3> list = new List<Vector3>();
+        List<Vector3> coneDirections = GetConeDirections(currentTarget, targeterFreePos, caster);
 
-	protected override void OnTargetSelModRemoved()
-	{
-		m_targetSelMod = null;
-	}
+        Vector3 reverseConeDir = -currentTarget.AimDirection;
+        reverseConeDir.Normalize();
+        Vector3 reverseConeRight = Vector3.Cross(reverseConeDir, Vector3.up).normalized;
 
-	private void SetCachedFields()
-	{
-		ConeTargetingInfo cachedConeInfo;
-		if (m_targetSelMod != null)
-		{
-			cachedConeInfo = m_targetSelMod.m_coneInfoMod.GetModifiedValue(m_coneInfo);
-		}
-		else
-		{
-			cachedConeInfo = m_coneInfo;
-		}
-		m_cachedConeInfo = cachedConeInfo;
-	}
+        float coneStartOffset = GetConeStartOffset() * Board.SquareSizeStatic;
+        Vector3 coneEndPos = targeterFreePos + coneStartOffset * reverseConeDir;
+        for (int i = 0; i < coneDirections.Count; i++)
+        {
+            float offset = GetPerConeHorizontalOffset() * (i - coneDirections.Count / 2);
+            list.Add(coneEndPos
+                     + reverseConeRight * offset
+                     - GetConeInfo().m_radiusInSquares * Board.SquareSizeStatic * coneDirections[i]);
+        }
 
-	public float GetLaserRange()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_laserRangeMod.GetModifiedValue(m_laserRange);
-		}
-		else
-		{
-			result = m_laserRange;
-		}
-		return result;
-	}
+        return list;
+    }
 
-	public float GetLaserWidth()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_laserWidthMod.GetModifiedValue(m_laserWidth);
-		}
-		else
-		{
-			result = m_laserWidth;
-		}
-		return result;
-	}
+    public virtual List<Vector3> GetConeDirections(
+        AbilityTarget currentTarget,
+        Vector3 targeterFreePos,
+        ActorData caster)
+    {
+        List<Vector3> list = new List<Vector3>();
+        int coneCount = GetConeCount();
+        float angleInBetween = GetAngleInBetween();
+        float aimAngle = VectorUtils.HorizontalAngle_Deg(currentTarget.AimDirection);
+        float startAngle = aimAngle + 0.5f * (coneCount - 1) * angleInBetween;
+        for (int i = 0; i < coneCount; i++)
+        {
+            list.Add(-VectorUtils.AngleDegreesToVector(startAngle - i * angleInBetween));
+        }
 
-	public ConeTargetingInfo GetConeInfo()
-	{
-		ConeTargetingInfo result;
-		if (m_cachedConeInfo != null)
-		{
-			result = m_cachedConeInfo;
-		}
-		else
-		{
-			result = m_coneInfo;
-		}
-		return result;
-	}
+        return list;
+    }
 
-	public int GetConeCount()
-	{
-		return (m_targetSelMod == null) ? m_coneCount : m_targetSelMod.m_coneCountMod.GetModifiedValue(m_coneCount);
-	}
-
-	public float GetConeStartOffset()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_coneStartOffsetMod.GetModifiedValue(m_coneStartOffset);
-		}
-		else
-		{
-			result = m_coneStartOffset;
-		}
-		return result;
-	}
-
-	public float GetPerConeHorizontalOffset()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_perConeHorizontalOffsetMod.GetModifiedValue(m_perConeHorizontalOffset);
-		}
-		else
-		{
-			result = m_perConeHorizontalOffset;
-		}
-		return result;
-	}
-
-	public float GetAngleInBetween()
-	{
-		float result;
-		if (m_targetSelMod != null)
-		{
-			result = m_targetSelMod.m_angleInBetweenMod.GetModifiedValue(m_angleInBetween);
-		}
-		else
-		{
-			result = m_angleInBetween;
-		}
-		return result;
-	}
-
-	public override ActorData.MovementType GetMovementType()
-	{
-		return ActorData.MovementType.Charge;
-	}
-
-	protected List<Vector3> GetConeOrigins(AbilityTarget currentTarget, Vector3 targeterFreePos, ActorData caster)
-	{
-		List<Vector3> list = new List<Vector3>();
-		List<Vector3> coneDirections = GetConeDirections(currentTarget, targeterFreePos, caster);
-		Vector3 vector = -currentTarget.AimDirection;
-		vector.Normalize();
-		Vector3 normalized = Vector3.Cross(vector, Vector3.up).normalized;
-		float d = GetConeStartOffset() * Board.SquareSizeStatic;
-		Vector3 a = targeterFreePos + d * vector;
-		for (int i = 0; i < coneDirections.Count; i++)
-		{
-			float d2 = GetPerConeHorizontalOffset() * (float)(i - coneDirections.Count / 2);
-			Vector3 item = a + normalized * d2;
-			item -= GetConeInfo().m_radiusInSquares * Board.SquareSizeStatic * coneDirections[i];
-			list.Add(item);
-		}
-		while (true)
-		{
-			return list;
-		}
-	}
-
-	public virtual List<Vector3> GetConeDirections(AbilityTarget currentTarget, Vector3 targeterFreePos, ActorData caster)
-	{
-		List<Vector3> list = new List<Vector3>();
-		int coneCount = GetConeCount();
-		float angleInBetween = GetAngleInBetween();
-		float num = VectorUtils.HorizontalAngle_Deg(currentTarget.AimDirection);
-		float num2 = num + 0.5f * (float)(coneCount - 1) * angleInBetween;
-		for (int i = 0; i < coneCount; i++)
-		{
-			list.Add(-VectorUtils.AngleDegreesToVector(num2 - (float)i * angleInBetween));
-		}
-		while (true)
-		{
-			return list;
-		}
-	}
-
-	public static bool CustomLosForCone(ActorData actor, ActorData caster, Vector3 chargeEndPos, List<NonActorTargetInfo> nonActorTargetInfo)
-	{
-		BoardSquare currentBoardSquare = actor.GetCurrentBoardSquare();
-		bool result = false;
-		BoardSquare boardSquare = Board.Get().GetSquareFromVec3(chargeEndPos);
-		if (boardSquare != null)
-		{
-			result = AreaEffectUtils.SquaresHaveLoSForAbilities(boardSquare, currentBoardSquare, caster, true, nonActorTargetInfo);
-		}
-		return result;
-	}
+    public static bool CustomLosForCone(
+        ActorData actor,
+        ActorData caster,
+        Vector3 chargeEndPos,
+        List<NonActorTargetInfo> nonActorTargetInfo)
+    {
+        BoardSquare chargeEndSquare = Board.Get().GetSquareFromVec3(chargeEndPos);
+        return chargeEndSquare != null && AreaEffectUtils.SquaresHaveLoSForAbilities(
+            chargeEndSquare,
+            actor.GetCurrentBoardSquare(),
+            caster,
+            true,
+            nonActorTargetInfo);
+    }
 }
