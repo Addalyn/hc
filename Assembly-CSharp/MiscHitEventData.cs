85757d45d5ad5a891be23bed938739e5a90afbc7
zheneq <zheneq@users.noreply.github.com>
2024-06-04 15:19:24 +0400

    Server update MiscHitEventData.cs

--- Assembly-CSharp/MiscHitEventData.cs
+++ Assembly-CSharp/MiscHitEventData.cs
@@ -1,50 +1,52 @@
 ﻿// ROGUES
 // SERVER
+
 using UnityEngine;
 
 // server-only, missing in reactor
 #if SERVER
 public class MiscHitEventData
 {
-	public MiscHitEventType m_type;
+    public MiscHitEventType m_type;
 
-	public MiscHitEventData(MiscHitEventType type)
-	{
-		this.m_type = type;
-	}
+    public MiscHitEventData(MiscHitEventType type)
+    {
+        m_type = type;
+    }
 
-	public virtual void ExecuteMiscHitEvent(ActorHitResults actorHitResult)
-	{
-		ActorHitParameters hitParameters = actorHitResult.m_hitParameters;
-		switch (this.m_type)
-		{
-		case MiscHitEventType.ClearCharacterAbilityCooldowns:
-			hitParameters.Target.GetAbilityData().ClearCharacterAbilityCooldowns();
-			return;
-		case MiscHitEventType.CasterForceChaseTarget:
-			if (!ServerActionBuffer.Get().HasPendingForcedChaseRequest(hitParameters.Caster))
-			{
-				ServerAbilityUtils.ForceChase(hitParameters.Caster, hitParameters.Target, true);
-				if (hitParameters.Ability != null)
-				{
-					AbilityData.ActionType actionTypeOfAbility = hitParameters.Caster.GetAbilityData().GetActionTypeOfAbility(hitParameters.Ability);
-					hitParameters.Caster.GetAbilityData().SetQueuedAction(actionTypeOfAbility, false);
-					return;
-				}
-			}
-			break;
-		case MiscHitEventType.TargetForceChaseCaster:
-			if (!hitParameters.Target.GetActorStatus().IsImmuneToForcedChase())
-			{
-				ServerAbilityUtils.ForceChase(hitParameters.Target, hitParameters.Caster, false);
-				hitParameters.Target.GetAbilityData().UnqueueActions();
-				return;
-			}
-			break;
-		default:
-			Debug.LogError("MiscHitEventData type cannot be handled by base class: " + this.m_type.ToString());
-			break;
-		}
-	}
+    public virtual void ExecuteMiscHitEvent(ActorHitResults actorHitResult)
+    {
+        ActorHitParameters hitParameters = actorHitResult.m_hitParameters;
+        switch (m_type)
+        {
+            case MiscHitEventType.ClearCharacterAbilityCooldowns:
+                hitParameters.Target.GetAbilityData().ClearCharacterAbilityCooldowns();
+                return;
+            case MiscHitEventType.CasterForceChaseTarget:
+                if (!ServerActionBuffer.Get().HasPendingForcedChaseRequest(hitParameters.Caster))
+                {
+                    ServerAbilityUtils.ForceChase(hitParameters.Caster, hitParameters.Target, true);
+                    if (hitParameters.Ability != null)
+                    {
+                        AbilityData.ActionType actionTypeOfAbility = hitParameters.Caster.GetAbilityData()
+                            .GetActionTypeOfAbility(hitParameters.Ability);
+                        hitParameters.Caster.GetAbilityData().SetQueuedAction(actionTypeOfAbility, false);
+                    }
+                }
+
+                break;
+            case MiscHitEventType.TargetForceChaseCaster:
+                if (!hitParameters.Target.GetActorStatus().IsImmuneToForcedChase())
+                {
+                    ServerAbilityUtils.ForceChase(hitParameters.Target, hitParameters.Caster, false);
+                    hitParameters.Target.GetAbilityData().UnqueueActions();
+                }
+
+                break;
+            default:
+                Debug.LogError("MiscHitEventData type cannot be handled by base class: " + m_type);
+                break;
+        }
+    }
 }
 #endif
