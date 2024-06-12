7264d409c992f186177e870c82a366d46f9078a6
zheneq <zheneq@users.noreply.github.com>
2024-06-13 00:46:09 +0400

    Update FireborgSuperheat.cs

--- Assembly-CSharp/FireborgSuperheat.cs
+++ Assembly-CSharp/FireborgSuperheat.cs
@@ -2,63 +2,47 @@ using System.Collections.Generic;
 
 public class FireborgSuperheat : GenericAbility_Container
 {
-	[Separator("Superheat", true)]
-	public int m_superheatDuration = 2;
+    [Separator("Superheat")]
+    public int m_superheatDuration = 2;
+    public int m_igniteExtraDamageIfSuperheated;
 
-	public int m_igniteExtraDamageIfSuperheated;
+    private Fireborg_SyncComponent m_syncComp;
+    private AbilityMod_FireborgSuperheat m_abilityMod;
 
-	private Fireborg_SyncComponent m_syncComp;
+    protected override void SetupTargetersAndCachedVars()
+    {
+        m_syncComp = GetComponent<Fireborg_SyncComponent>();
+        base.SetupTargetersAndCachedVars();
+    }
 
-	private AbilityMod_FireborgSuperheat m_abilityMod;
+    protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
+    {
+        base.AddSpecificTooltipTokens(tokens, modAsBase);
+        AddTokenInt(tokens, "SuperheatDuration", string.Empty, m_superheatDuration);
+        AddTokenInt(tokens, "IgniteExtraDamageIfSuperheated", string.Empty, m_igniteExtraDamageIfSuperheated);
+    }
 
-	protected override void SetupTargetersAndCachedVars()
-	{
-		m_syncComp = GetComponent<Fireborg_SyncComponent>();
-		base.SetupTargetersAndCachedVars();
-	}
+    public int GetSuperheatDuration()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_superheatDurationMod.GetModifiedValue(m_superheatDuration)
+            : m_superheatDuration;
+    }
 
-	protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
-	{
-		base.AddSpecificTooltipTokens(tokens, modAsBase);
-		AddTokenInt(tokens, "SuperheatDuration", string.Empty, m_superheatDuration);
-		AddTokenInt(tokens, "IgniteExtraDamageIfSuperheated", string.Empty, m_igniteExtraDamageIfSuperheated);
-	}
+    public int GetIgniteExtraDamageIfSuperheated()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_igniteExtraDamageIfSuperheatedMod.GetModifiedValue(m_igniteExtraDamageIfSuperheated)
+            : m_igniteExtraDamageIfSuperheated;
+    }
 
-	public int GetSuperheatDuration()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_superheatDurationMod.GetModifiedValue(m_superheatDuration);
-		}
-		else
-		{
-			result = m_superheatDuration;
-		}
-		return result;
-	}
+    protected override void GenModImpl_SetModRef(AbilityMod abilityMod)
+    {
+        m_abilityMod = abilityMod as AbilityMod_FireborgSuperheat;
+    }
 
-	public int GetIgniteExtraDamageIfSuperheated()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_igniteExtraDamageIfSuperheatedMod.GetModifiedValue(m_igniteExtraDamageIfSuperheated);
-		}
-		else
-		{
-			result = m_igniteExtraDamageIfSuperheated;
-		}
-		return result;
-	}
-
-	protected override void GenModImpl_SetModRef(AbilityMod abilityMod)
-	{
-		m_abilityMod = (abilityMod as AbilityMod_FireborgSuperheat);
-	}
-
-	protected override void GenModImpl_ClearModRef()
-	{
-		m_abilityMod = null;
-	}
+    protected override void GenModImpl_ClearModRef()
+    {
+        m_abilityMod = null;
+    }
 }
