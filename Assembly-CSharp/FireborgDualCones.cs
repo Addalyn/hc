8360c39175076508324937fc0b6a44a4a8cd097c
zheneq <zheneq@users.noreply.github.com>
2024-06-12 19:44:23 +0400

    Update FireborgDualCones.cs

--- Assembly-CSharp/FireborgDualCones.cs
+++ Assembly-CSharp/FireborgDualCones.cs
@@ -1,303 +1,199 @@
-using AbilityContextNamespace;
 using System.Collections.Generic;
+using AbilityContextNamespace;
 using UnityEngine;
 
 public class FireborgDualCones : GenericAbility_Container
 {
-	[Separator("Extra Damage for overlap state", true)]
-	public int m_extraDamageIfOverlap;
+    [Separator("Extra Damage for overlap state")]
+    public int m_extraDamageIfOverlap;
+    public int m_extraDamageNonOverlap;
+    [Separator("Add Ignited Effect If Overlap Hit")]
+    public bool m_igniteTargetIfOverlapHit = true;
+    public bool m_igniteTargetIfSuperheated = true;
+    [Separator("Ground Fire")]
+    public bool m_groundFireOnAllIfNormal;
+    public bool m_groundFireOnOverlapIfNormal;
+    [Space(10f)]
+    public bool m_groundFireOnAllIfSuperheated = true;
+    public bool m_groundFireOnOverlapIfSuperheated = true;
+    [Separator("Superheat Sequence")]
+    public GameObject m_superheatCastSeqPrefab;
 
-	public int m_extraDamageNonOverlap;
+    private Fireborg_SyncComponent m_syncComp;
+    private AbilityMod_FireborgDualCones m_abilityMod;
 
-	[Separator("Add Ignited Effect If Overlap Hit", true)]
-	public bool m_igniteTargetIfOverlapHit = true;
+    public override string GetUsageForEditor()
+    {
+        return base.GetUsageForEditor() + Fireborg_SyncComponent.GetSuperheatedCvarUsage();
+    }
 
-	public bool m_igniteTargetIfSuperheated = true;
+    public override List<string> GetContextNamesForEditor()
+    {
+        List<string> contextNamesForEditor = base.GetContextNamesForEditor();
+        contextNamesForEditor.Add(Fireborg_SyncComponent.s_cvarSuperheated.GetName());
+        return contextNamesForEditor;
+    }
 
-	[Separator("Ground Fire", true)]
-	public bool m_groundFireOnAllIfNormal;
+    protected override void SetupTargetersAndCachedVars()
+    {
+        m_syncComp = GetComponent<Fireborg_SyncComponent>();
+        base.SetupTargetersAndCachedVars();
+    }
 
-	public bool m_groundFireOnOverlapIfNormal;
+    protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
+    {
+        base.AddSpecificTooltipTokens(tokens, modAsBase);
+        AddTokenInt(tokens, "ExtraDamageIfOverlap", string.Empty, m_extraDamageIfOverlap);
+        AddTokenInt(tokens, "ExtraDamageNonOverlap", string.Empty, m_extraDamageNonOverlap);
+    }
 
-	[Space(10f)]
-	public bool m_groundFireOnAllIfSuperheated = true;
+    public int GetExtraDamageIfOverlap()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_extraDamageIfOverlapMod.GetModifiedValue(m_extraDamageIfOverlap)
+            : m_extraDamageIfOverlap;
+    }
 
-	public bool m_groundFireOnOverlapIfSuperheated = true;
+    public int GetExtraDamageNonOverlap()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_extraDamageNonOverlapMod.GetModifiedValue(m_extraDamageNonOverlap)
+            : m_extraDamageNonOverlap;
+    }
 
-	[Separator("Superheat Sequence", true)]
-	public GameObject m_superheatCastSeqPrefab;
+    public bool IgniteTargetIfOverlapHit()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_igniteTargetIfOverlapHitMod.GetModifiedValue(m_igniteTargetIfOverlapHit)
+            : m_igniteTargetIfOverlapHit;
+    }
 
-	private Fireborg_SyncComponent m_syncComp;
+    public bool IgniteTargetIfSuperheated()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_igniteTargetIfSuperheatedMod.GetModifiedValue(m_igniteTargetIfSuperheated)
+            : m_igniteTargetIfSuperheated;
+    }
 
-	private AbilityMod_FireborgDualCones m_abilityMod;
+    public bool GroundFireOnAllIfNormal()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_groundFireOnAllIfNormalMod.GetModifiedValue(m_groundFireOnAllIfNormal)
+            : m_groundFireOnAllIfNormal;
+    }
 
-	public override string GetUsageForEditor()
-	{
-		string usageForEditor = base.GetUsageForEditor();
-		return usageForEditor + Fireborg_SyncComponent.GetSuperheatedCvarUsage();
-	}
+    public bool GroundFireOnOverlapIfNormal()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_groundFireOnOverlapIfNormalMod.GetModifiedValue(m_groundFireOnOverlapIfNormal)
+            : m_groundFireOnOverlapIfNormal;
+    }
 
-	public override List<string> GetContextNamesForEditor()
-	{
-		List<string> contextNamesForEditor = base.GetContextNamesForEditor();
-		contextNamesForEditor.Add(Fireborg_SyncComponent.s_cvarSuperheated.GetName());
-		return contextNamesForEditor;
-	}
+    public bool GroundFireOnAllIfSuperheated()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_groundFireOnAllIfSuperheatedMod.GetModifiedValue(m_groundFireOnAllIfSuperheated)
+            : m_groundFireOnAllIfSuperheated;
+    }
 
-	protected override void SetupTargetersAndCachedVars()
-	{
-		m_syncComp = GetComponent<Fireborg_SyncComponent>();
-		base.SetupTargetersAndCachedVars();
-	}
+    public bool GroundFireOnOverlapIfSuperheated()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_groundFireOnOverlapIfSuperheatedMod.GetModifiedValue(m_groundFireOnOverlapIfSuperheated)
+            : m_groundFireOnOverlapIfSuperheated;
+    }
 
-	protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
-	{
-		base.AddSpecificTooltipTokens(tokens, modAsBase);
-		AddTokenInt(tokens, "ExtraDamageIfOverlap", string.Empty, m_extraDamageIfOverlap);
-		AddTokenInt(tokens, "ExtraDamageNonOverlap", string.Empty, m_extraDamageNonOverlap);
-	}
+    public override void PreProcessTargetingNumbers(
+        ActorData targetActor,
+        int currentTargetIndex,
+        Dictionary<ActorData, ActorHitContext> actorHitContext,
+        ContextVars abilityContext)
+    {
+        m_syncComp.SetSuperheatedContextVar(abilityContext);
+    }
 
-	public int GetExtraDamageIfOverlap()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_extraDamageIfOverlapMod.GetModifiedValue(m_extraDamageIfOverlap);
-		}
-		else
-		{
-			result = m_extraDamageIfOverlap;
-		}
-		return result;
-	}
+    public override void PostProcessTargetingNumbers(
+        ActorData targetActor,
+        int currentTargeterIndex,
+        Dictionary<ActorData, ActorHitContext> actorHitContext,
+        ContextVars abilityContext,
+        ActorData caster,
+        TargetingNumberUpdateScratch results)
+    {
+        if (GetExtraDamageIfOverlap() <= 0 && GetExtraDamageNonOverlap() <= 0)
+        {
+            return;
+        }
 
-	public int GetExtraDamageNonOverlap()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_extraDamageNonOverlapMod.GetModifiedValue(m_extraDamageNonOverlap);
-		}
-		else
-		{
-			result = m_extraDamageNonOverlap;
-		}
-		return result;
-	}
+        if (!actorHitContext.ContainsKey(targetActor))
+        {
+            return;
+        }
 
-	public bool IgniteTargetIfOverlapHit()
-	{
-		bool result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_igniteTargetIfOverlapHitMod.GetModifiedValue(m_igniteTargetIfOverlapHit);
-		}
-		else
-		{
-			result = m_igniteTargetIfOverlapHit;
-		}
-		return result;
-	}
+        actorHitContext[targetActor].m_contextVars.TryGetInt(ContextKeys.s_HitCount.GetKey(), out int hitCount);
+        if (hitCount > 1)
+        {
+            if (GetExtraDamageIfOverlap() > 0)
+            {
+                results.m_damage += GetExtraDamageIfOverlap();
+            }
+        }
+        else
+        {
+            if (GetExtraDamageNonOverlap() > 0)
+            {
+                results.m_damage += GetExtraDamageNonOverlap();
+            }
+        }
+    }
 
-	public bool IgniteTargetIfSuperheated()
-	{
-		return (!(m_abilityMod != null)) ? m_igniteTargetIfSuperheated : m_abilityMod.m_igniteTargetIfSuperheatedMod.GetModifiedValue(m_igniteTargetIfSuperheated);
-	}
+    public override string GetAccessoryTargeterNumberString(
+        ActorData targetActor,
+        AbilityTooltipSymbol symbolType,
+        int baseValue)
+    {
+        if (ShouldAddGroundFire()
+            && (ShouldAddGroundFireToAllSquares() || GetTargeterHitCountOnTarget(targetActor) > 1)
+            && !m_syncComp.m_actorsInGroundFireOnTurnStart.Contains((uint)targetActor.ActorIndex))
+        {
+            return m_syncComp.GetTargetPreviewAccessoryString(symbolType, this, targetActor, ActorData);
+        }
 
-	public bool GroundFireOnAllIfNormal()
-	{
-		bool result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_groundFireOnAllIfNormalMod.GetModifiedValue(m_groundFireOnAllIfNormal);
-		}
-		else
-		{
-			result = m_groundFireOnAllIfNormal;
-		}
-		return result;
-	}
+        return null;
+    }
 
-	public bool GroundFireOnOverlapIfNormal()
-	{
-		return (!(m_abilityMod != null)) ? m_groundFireOnOverlapIfNormal : m_abilityMod.m_groundFireOnOverlapIfNormalMod.GetModifiedValue(m_groundFireOnOverlapIfNormal);
-	}
+    private int GetTargeterHitCountOnTarget(ActorData targetActor)
+    {
+        if (Targeter.GetActorContextVars().TryGetValue(targetActor, out ActorHitContext actorHitContext)
+            && actorHitContext.m_contextVars.TryGetInt(ContextKeys.s_HitCount.GetKey(), out int hitCount))
+        {
+            return hitCount;
+        }
 
-	public bool GroundFireOnAllIfSuperheated()
-	{
-		bool result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_groundFireOnAllIfSuperheatedMod.GetModifiedValue(m_groundFireOnAllIfSuperheated);
-		}
-		else
-		{
-			result = m_groundFireOnAllIfSuperheated;
-		}
-		return result;
-	}
+        return 0;
+    }
 
-	public bool GroundFireOnOverlapIfSuperheated()
-	{
-		return (!(m_abilityMod != null)) ? m_groundFireOnOverlapIfSuperheated : m_abilityMod.m_groundFireOnOverlapIfSuperheatedMod.GetModifiedValue(m_groundFireOnOverlapIfSuperheated);
-	}
+    private bool ShouldAddGroundFire()
+    {
+        return m_syncComp.InSuperheatMode()
+            ? GroundFireOnAllIfSuperheated() || GroundFireOnOverlapIfSuperheated()
+            : GroundFireOnAllIfNormal() || GroundFireOnOverlapIfNormal();
+    }
 
-	public override void PreProcessTargetingNumbers(ActorData targetActor, int currentTargetIndex, Dictionary<ActorData, ActorHitContext> actorHitContext, ContextVars abilityContext)
-	{
-		m_syncComp.SetSuperheatedContextVar(abilityContext);
-	}
+    private bool ShouldAddGroundFireToAllSquares()
+    {
+        return m_syncComp.InSuperheatMode()
+            ? GroundFireOnAllIfSuperheated()
+            : GroundFireOnAllIfNormal();
+    }
 
-	public override void PostProcessTargetingNumbers(ActorData targetActor, int currentTargeterIndex, Dictionary<ActorData, ActorHitContext> actorHitContext, ContextVars abilityContext, ActorData caster, TargetingNumberUpdateScratch results)
-	{
-		if (GetExtraDamageIfOverlap() <= 0)
-		{
-			if (GetExtraDamageNonOverlap() <= 0)
-			{
-				return;
-			}
-		}
-		if (!actorHitContext.ContainsKey(targetActor))
-		{
-			return;
-		}
-		while (true)
-		{
-			actorHitContext[targetActor].m_contextVars.TryGetInt(ContextKeys.s_HitCount.GetKey(), out int value);
-			if (value > 1)
-			{
-				while (true)
-				{
-					switch (4)
-					{
-					case 0:
-						break;
-					default:
-						if (GetExtraDamageIfOverlap() > 0)
-						{
-							results.m_damage += GetExtraDamageIfOverlap();
-						}
-						return;
-					}
-				}
-			}
-			if (GetExtraDamageNonOverlap() > 0)
-			{
-				while (true)
-				{
-					results.m_damage += GetExtraDamageNonOverlap();
-					return;
-				}
-			}
-			return;
-		}
-	}
+    protected override void GenModImpl_SetModRef(AbilityMod abilityMod)
+    {
+        m_abilityMod = abilityMod as AbilityMod_FireborgDualCones;
+    }
 
-	public override string GetAccessoryTargeterNumberString(ActorData targetActor, AbilityTooltipSymbol symbolType, int baseValue)
-	{
-		if (ShouldAddGroundFire())
-		{
-			int num;
-			if (!ShouldAddGroundFireToAllSquares())
-			{
-				num = ((GetTargeterHitCountOnTarget(targetActor) > 1) ? 1 : 0);
-			}
-			else
-			{
-				num = 1;
-			}
-			if (num != 0)
-			{
-				if (!m_syncComp.m_actorsInGroundFireOnTurnStart.Contains((uint)targetActor.ActorIndex))
-				{
-					while (true)
-					{
-						switch (2)
-						{
-						case 0:
-							break;
-						default:
-							return m_syncComp.GetTargetPreviewAccessoryString(symbolType, this, targetActor, base.ActorData);
-						}
-					}
-				}
-			}
-		}
-		return null;
-	}
-
-	private int GetTargeterHitCountOnTarget(ActorData targetActor)
-	{
-		if (base.Targeter.GetActorContextVars().TryGetValue(targetActor, out ActorHitContext value))
-		{
-			if (value.m_contextVars.TryGetInt(ContextKeys.s_HitCount.GetKey(), out int value2))
-			{
-				while (true)
-				{
-					switch (1)
-					{
-					case 0:
-						break;
-					default:
-						return value2;
-					}
-				}
-			}
-		}
-		return 0;
-	}
-
-	private bool ShouldAddGroundFire()
-	{
-		if (m_syncComp.InSuperheatMode())
-		{
-			while (true)
-			{
-				switch (1)
-				{
-				case 0:
-					break;
-				default:
-					return GroundFireOnAllIfSuperheated() || GroundFireOnOverlapIfSuperheated();
-				}
-			}
-		}
-		int result;
-		if (!GroundFireOnAllIfNormal())
-		{
-			result = (GroundFireOnOverlapIfNormal() ? 1 : 0);
-		}
-		else
-		{
-			result = 1;
-		}
-		return (byte)result != 0;
-	}
-
-	private bool ShouldAddGroundFireToAllSquares()
-	{
-		if (m_syncComp.InSuperheatMode())
-		{
-			while (true)
-			{
-				switch (4)
-				{
-				case 0:
-					break;
-				default:
-					return GroundFireOnAllIfSuperheated();
-				}
-			}
-		}
-		return GroundFireOnAllIfNormal();
-	}
-
-	protected override void GenModImpl_SetModRef(AbilityMod abilityMod)
-	{
-		m_abilityMod = (abilityMod as AbilityMod_FireborgDualCones);
-	}
-
-	protected override void GenModImpl_ClearModRef()
-	{
-		m_abilityMod = null;
-	}
+    protected override void GenModImpl_ClearModRef()
+    {
+        m_abilityMod = null;
+    }
 }
