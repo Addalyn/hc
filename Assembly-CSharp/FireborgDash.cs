70d5b6f25642e5e19412b4e440b41c6258635a00
zheneq <zheneq@users.noreply.github.com>
2024-06-13 22:11:21 +0400

    Update FireborgDash.cs

--- Assembly-CSharp/FireborgDash.cs
+++ Assembly-CSharp/FireborgDash.cs
@@ -1,204 +1,162 @@
-using AbilityContextNamespace;
 using System.Collections.Generic;
+using AbilityContextNamespace;
 using UnityEngine;
 
 public class FireborgDash : GenericAbility_Container
 {
-	[Separator("Whether to add ground fire effect", true)]
-	public bool m_addGroundFire = true;
+    [Separator("Whether to add ground fire effect")]
+    public bool m_addGroundFire = true;
+    public int m_groundFireDuration = 1;
+    public int m_groundFireDurationIfSuperheated = 1;
+    public bool m_igniteIfNormal;
+    public bool m_igniteIfSuperheated = true;
+    [Separator("Shield per Enemy Hit")]
+    public int m_shieldPerEnemyHit;
+    public int m_shieldDuration = 1;
+    [Separator("Cooldown Reduction")]
+    public int m_cdrPerTurnIfLowHealth;
+    public int m_lowHealthThresh;
+    [Separator("Sequence")]
+    public GameObject m_superheatedCastSeqPrefab;
 
-	public int m_groundFireDuration = 1;
+    private Fireborg_SyncComponent m_syncComp;
+    private AbilityMod_FireborgDash m_abilityMod;
 
-	public int m_groundFireDurationIfSuperheated = 1;
+    public override string GetUsageForEditor()
+    {
+        return base.GetUsageForEditor() + Fireborg_SyncComponent.GetSuperheatedCvarUsage();
+    }
 
-	public bool m_igniteIfNormal;
+    public override List<string> GetContextNamesForEditor()
+    {
+        List<string> contextNamesForEditor = base.GetContextNamesForEditor();
+        contextNamesForEditor.Add(Fireborg_SyncComponent.s_cvarSuperheated.GetName());
+        return contextNamesForEditor;
+    }
 
-	public bool m_igniteIfSuperheated = true;
+    protected override void SetupTargetersAndCachedVars()
+    {
+        m_syncComp = GetComponent<Fireborg_SyncComponent>();
+        base.SetupTargetersAndCachedVars();
+    }
 
-	[Separator("Shield per Enemy Hit", true)]
-	public int m_shieldPerEnemyHit;
+    protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
+    {
+        base.AddSpecificTooltipTokens(tokens, modAsBase);
+        AddTokenInt(tokens, "GroundFireDuration", string.Empty, m_groundFireDuration);
+        AddTokenInt(tokens, "GroundFireDurationIfSuperheated", string.Empty, m_groundFireDurationIfSuperheated);
+        AddTokenInt(tokens, "ShieldPerEnemyHit", string.Empty, m_shieldPerEnemyHit);
+        AddTokenInt(tokens, "ShieldDuration", string.Empty, m_shieldDuration);
+        AddTokenInt(tokens, "CdrPerTurnIfLowHealth", string.Empty, m_cdrPerTurnIfLowHealth);
+        AddTokenInt(tokens, "LowHealthThresh", string.Empty, m_lowHealthThresh);
+    }
 
-	public int m_shieldDuration = 1;
+    public bool AddGroundFire()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_addGroundFireMod.GetModifiedValue(m_addGroundFire)
+            : m_addGroundFire;
+    }
 
-	[Separator("Cooldown Reduction", true)]
-	public int m_cdrPerTurnIfLowHealth;
+    public int GetGroundFireDuration()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_groundFireDurationMod.GetModifiedValue(m_groundFireDuration)
+            : m_groundFireDuration;
+    }
 
-	public int m_lowHealthThresh;
+    public int GetGroundFireDurationIfSuperheated()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_groundFireDurationIfSuperheatedMod.GetModifiedValue(m_groundFireDurationIfSuperheated)
+            : m_groundFireDurationIfSuperheated;
+    }
 
-	[Separator("Sequence", true)]
-	public GameObject m_superheatedCastSeqPrefab;
+    public bool IgniteIfNormal()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_igniteIfNormalMod.GetModifiedValue(m_igniteIfNormal)
+            : m_igniteIfNormal;
+    }
 
-	private Fireborg_SyncComponent m_syncComp;
+    public bool IgniteIfSuperheated()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_igniteIfSuperheatedMod.GetModifiedValue(m_igniteIfSuperheated)
+            : m_igniteIfSuperheated;
+    }
 
-	private AbilityMod_FireborgDash m_abilityMod;
+    public int GetShieldPerEnemyHit()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_shieldPerEnemyHitMod.GetModifiedValue(m_shieldPerEnemyHit)
+            : m_shieldPerEnemyHit;
+    }
 
-	public override string GetUsageForEditor()
-	{
-		string usageForEditor = base.GetUsageForEditor();
-		return usageForEditor + Fireborg_SyncComponent.GetSuperheatedCvarUsage();
-	}
+    public int GetShieldDuration()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_shieldDurationMod.GetModifiedValue(m_shieldDuration)
+            : m_shieldDuration;
+    }
 
-	public override List<string> GetContextNamesForEditor()
-	{
-		List<string> contextNamesForEditor = base.GetContextNamesForEditor();
-		contextNamesForEditor.Add(Fireborg_SyncComponent.s_cvarSuperheated.GetName());
-		return contextNamesForEditor;
-	}
+    public int GetCdrPerTurnIfLowHealth()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_cdrPerTurnIfLowHealthMod.GetModifiedValue(m_cdrPerTurnIfLowHealth)
+            : m_cdrPerTurnIfLowHealth;
+    }
 
-	protected override void SetupTargetersAndCachedVars()
-	{
-		m_syncComp = GetComponent<Fireborg_SyncComponent>();
-		base.SetupTargetersAndCachedVars();
-	}
+    public int GetLowHealthThresh()
+    {
+        return m_abilityMod != null
+            ? m_abilityMod.m_lowHealthThreshMod.GetModifiedValue(m_lowHealthThresh)
+            : m_lowHealthThresh;
+    }
 
-	protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
-	{
-		base.AddSpecificTooltipTokens(tokens, modAsBase);
-		AddTokenInt(tokens, "GroundFireDuration", string.Empty, m_groundFireDuration);
-		AddTokenInt(tokens, "GroundFireDurationIfSuperheated", string.Empty, m_groundFireDurationIfSuperheated);
-		AddTokenInt(tokens, "ShieldPerEnemyHit", string.Empty, m_shieldPerEnemyHit);
-		AddTokenInt(tokens, "ShieldDuration", string.Empty, m_shieldDuration);
-		AddTokenInt(tokens, "CdrPerTurnIfLowHealth", string.Empty, m_cdrPerTurnIfLowHealth);
-		AddTokenInt(tokens, "LowHealthThresh", string.Empty, m_lowHealthThresh);
-	}
+    public override void PreProcessTargetingNumbers(
+        ActorData targetActor,
+        int currentTargetIndex,
+        Dictionary<ActorData, ActorHitContext> actorHitContext,
+        ContextVars abilityContext)
+    {
+        m_syncComp.SetSuperheatedContextVar(abilityContext);
+    }
 
-	public bool AddGroundFire()
-	{
-		bool result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_addGroundFireMod.GetModifiedValue(m_addGroundFire);
-		}
-		else
-		{
-			result = m_addGroundFire;
-		}
-		return result;
-	}
+    public override void PostProcessTargetingNumbers(
+        ActorData targetActor,
+        int currentTargeterIndex,
+        Dictionary<ActorData, ActorHitContext> actorHitContext,
+        ContextVars abilityContext,
+        ActorData caster,
+        TargetingNumberUpdateScratch results)
+    {
+        IceborgConeOrLaser.SetShieldPerEnemyHitTargetingNumbers(
+            targetActor,
+            caster,
+            GetShieldPerEnemyHit(),
+            actorHitContext,
+            results);
+    }
 
-	public int GetGroundFireDuration()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_groundFireDurationMod.GetModifiedValue(m_groundFireDuration);
-		}
-		else
-		{
-			result = m_groundFireDuration;
-		}
-		return result;
-	}
+    public override string GetAccessoryTargeterNumberString(
+        ActorData targetActor,
+        AbilityTooltipSymbol symbolType,
+        int baseValue)
+    {
+        return AddGroundFire()
+               && !m_syncComp.m_actorsInGroundFireOnTurnStart.Contains((uint)targetActor.ActorIndex)
+            ? m_syncComp.GetTargetPreviewAccessoryString(symbolType, this, targetActor, ActorData)
+            : null;
+    }
 
-	public int GetGroundFireDurationIfSuperheated()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_groundFireDurationIfSuperheatedMod.GetModifiedValue(m_groundFireDurationIfSuperheated);
-		}
-		else
-		{
-			result = m_groundFireDurationIfSuperheated;
-		}
-		return result;
-	}
+    protected override void GenModImpl_SetModRef(AbilityMod abilityMod)
+    {
+        m_abilityMod = abilityMod as AbilityMod_FireborgDash;
+    }
 
-	public bool IgniteIfNormal()
-	{
-		return (!(m_abilityMod != null)) ? m_igniteIfNormal : m_abilityMod.m_igniteIfNormalMod.GetModifiedValue(m_igniteIfNormal);
-	}
-
-	public bool IgniteIfSuperheated()
-	{
-		bool result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_igniteIfSuperheatedMod.GetModifiedValue(m_igniteIfSuperheated);
-		}
-		else
-		{
-			result = m_igniteIfSuperheated;
-		}
-		return result;
-	}
-
-	public int GetShieldPerEnemyHit()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_shieldPerEnemyHitMod.GetModifiedValue(m_shieldPerEnemyHit);
-		}
-		else
-		{
-			result = m_shieldPerEnemyHit;
-		}
-		return result;
-	}
-
-	public int GetShieldDuration()
-	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_shieldDurationMod.GetModifiedValue(m_shieldDuration);
-		}
-		else
-		{
-			result = m_shieldDuration;
-		}
-		return result;
-	}
-
-	public int GetCdrPerTurnIfLowHealth()
-	{
-		return (!(m_abilityMod != null)) ? m_cdrPerTurnIfLowHealth : m_abilityMod.m_cdrPerTurnIfLowHealthMod.GetModifiedValue(m_cdrPerTurnIfLowHealth);
-	}
-
-	public int GetLowHealthThresh()
-	{
-		return (!(m_abilityMod != null)) ? m_lowHealthThresh : m_abilityMod.m_lowHealthThreshMod.GetModifiedValue(m_lowHealthThresh);
-	}
-
-	public override void PreProcessTargetingNumbers(ActorData targetActor, int currentTargetIndex, Dictionary<ActorData, ActorHitContext> actorHitContext, ContextVars abilityContext)
-	{
-		m_syncComp.SetSuperheatedContextVar(abilityContext);
-	}
-
-	public override void PostProcessTargetingNumbers(ActorData targetActor, int currentTargeterIndex, Dictionary<ActorData, ActorHitContext> actorHitContext, ContextVars abilityContext, ActorData caster, TargetingNumberUpdateScratch results)
-	{
-		IceborgConeOrLaser.SetShieldPerEnemyHitTargetingNumbers(targetActor, caster, GetShieldPerEnemyHit(), actorHitContext, results);
-	}
-
-	public override string GetAccessoryTargeterNumberString(ActorData targetActor, AbilityTooltipSymbol symbolType, int baseValue)
-	{
-		if (AddGroundFire())
-		{
-			if (!m_syncComp.m_actorsInGroundFireOnTurnStart.Contains((uint)targetActor.ActorIndex))
-			{
-				while (true)
-				{
-					switch (6)
-					{
-					case 0:
-						break;
-					default:
-						return m_syncComp.GetTargetPreviewAccessoryString(symbolType, this, targetActor, base.ActorData);
-					}
-				}
-			}
-		}
-		return null;
-	}
-
-	protected override void GenModImpl_SetModRef(AbilityMod abilityMod)
-	{
-		m_abilityMod = (abilityMod as AbilityMod_FireborgDash);
-	}
-
-	protected override void GenModImpl_ClearModRef()
-	{
-		m_abilityMod = null;
-	}
+    protected override void GenModImpl_ClearModRef()
+    {
+        m_abilityMod = null;
+    }
 }
