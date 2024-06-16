47eed8fef72a0b16997e19ab4ef392e188b452e4
zheneq <zheneq@users.noreply.github.com>
2024-06-16 06:05:37 +0400

    Server update MiscHitEventData_ProgressCasterStockRefreshTime

--- Assembly-CSharp/MiscHitEventData_ProgressCasterStockRefreshTime.cs
+++ Assembly-CSharp/MiscHitEventData_ProgressCasterStockRefreshTime.cs
@@ -5,25 +5,27 @@
 public class MiscHitEventData_ProgressCasterStockRefreshTime : MiscHitEventData
 {
 	public AbilityData.ActionType m_actionType;
-
 	public int m_progressAmount;
 
-	public MiscHitEventData_ProgressCasterStockRefreshTime(AbilityData.ActionType actionType, int progressAmount) : base(MiscHitEventType.ProgressCasterStockRefreshTime)
+	public MiscHitEventData_ProgressCasterStockRefreshTime(AbilityData.ActionType actionType, int progressAmount)
+		: base(MiscHitEventType.ProgressCasterStockRefreshTime)
 	{
-		this.m_actionType = actionType;
-		this.m_progressAmount = progressAmount;
+		m_actionType = actionType;
+		m_progressAmount = progressAmount;
 	}
 
 	public override void ExecuteMiscHitEvent(ActorHitResults actorHitResult)
 	{
-		if (this.m_actionType != AbilityData.ActionType.INVALID_ACTION && this.m_progressAmount > 0)
+		if (m_actionType == AbilityData.ActionType.INVALID_ACTION || m_progressAmount <= 0)
 		{
-			AbilityData abilityData = actorHitResult.m_hitParameters.Caster.GetAbilityData();
-			if (abilityData != null && abilityData.GetAbilityOfActionType(this.m_actionType) != null)
-			{
-				abilityData.ProgressStockRefreshTimeForAction(this.m_actionType, this.m_progressAmount);
-			}
+			return;
 		}
+		AbilityData abilityData = actorHitResult.m_hitParameters.Caster.GetAbilityData();
+		if (abilityData == null || abilityData.GetAbilityOfActionType(m_actionType) == null)
+		{
+			return;
+		}
+		abilityData.ProgressStockRefreshTimeForAction(m_actionType, m_progressAmount);
 	}
 }
 #endif
