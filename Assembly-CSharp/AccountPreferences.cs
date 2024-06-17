06748a949b98261910827f4730e95caa17b1c571
zheneq <zheneq@users.noreply.github.com>
2024-06-17 09:26:59 +0400

    Update AccountPreferences.cs

--- Assembly-CSharp/AccountPreferences.cs
+++ Assembly-CSharp/AccountPreferences.cs
@@ -4,171 +4,120 @@ using UnityEngine;
 
 public class AccountPreferences : MonoBehaviour
 {
-	[Serializable]
-	public class BoolDefault
-	{
-		public BoolPreference m_preference;
+    [Serializable]
+    public class BoolDefault
+    {
+        public BoolPreference m_preference;
+        public bool m_value;
+    }
 
-		public bool m_value;
-	}
+    [Serializable]
+    public class KeyCodeDefault
+    {
+        public KeyPreference m_preference;
+        public KeyCode m_primary;
+        public KeyCode m_modifierKey1;
+        public KeyCode m_additionalModifierKey1;
+        public KeyCode m_secondary;
+        public KeyCode m_modifierKey2;
+        public KeyCode m_additionalModifierKey2;
+    }
 
-	[Serializable]
-	public class KeyCodeDefault
-	{
-		public KeyPreference m_preference;
+    [Serializable]
+    public class DefaultSettingsData
+    {
+        public string Name;
+        public BoolDefault[] m_boolDefaults = new BoolDefault[0];
+        public KeyCodeDefault[] m_keyDefaults = new KeyCodeDefault[0];
+    }
 
-		public KeyCode m_primary;
+    internal bool ApplicationHasFocus = true;
+    public DefaultSettingsData[] m_defaultSettings;
+    public int m_languageIndex;
 
-		public KeyCode m_modifierKey1;
+    private static AccountPreferences s_instance;
 
-		public KeyCode m_additionalModifierKey1;
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        ApplicationHasFocus = hasFocus;
+    }
 
-		public KeyCode m_secondary;
+    internal static AccountPreferences Get()
+    {
+        return s_instance;
+    }
 
-		public KeyCode m_modifierKey2;
+    public static bool DoesApplicationHaveFocus()
+    {
+        return s_instance != null && s_instance.ApplicationHasFocus;
+    }
 
-		public KeyCode m_additionalModifierKey2;
-	}
+    private void Awake()
+    {
+        s_instance = this;
+    }
 
-	[Serializable]
-	public class DefaultSettingsData
-	{
-		public string Name;
+    private void Start()
+    {
+        SetLanguageIndex();
+        foreach (BoolDefault value in m_defaultSettings[m_languageIndex].m_boolDefaults)
+        {
+            if (!PlayerPrefs.HasKey(value.m_preference.ToString()))
+            {
+                SetBool(value.m_preference, value.m_value);
+            }
+        }
+    }
 
-		public BoolDefault[] m_boolDefaults = new BoolDefault[0];
+    private void OnDestroy()
+    {
+        s_instance = null;
+    }
 
-		public KeyCodeDefault[] m_keyDefaults = new KeyCodeDefault[0];
-	}
+    private void SetLanguageIndex()
+    {
+        m_languageIndex = 0;
+        int languageIndex = 0;
 
-	internal bool ApplicationHasFocus = true;
+        foreach (DefaultSettingsData defaultSettingsData in m_defaultSettings)
+        {
+            if (defaultSettingsData.Name == LocalizationManager.CurrentLanguageCode)
+            {
+                m_languageIndex = languageIndex;
+                return;
+            }
 
-	public DefaultSettingsData[] m_defaultSettings;
+            languageIndex++;
+        }
+    }
 
-	public int m_languageIndex;
+    public int GetKeyDefaultsSize()
+    {
+        return m_defaultSettings[m_languageIndex].m_keyDefaults.Length;
+    }
 
-	private static AccountPreferences s_instance;
+    public KeyCodeDefault GetKeyCodeDefault(int index)
+    {
+        return m_defaultSettings[m_languageIndex].m_keyDefaults[index];
+    }
 
-	private void OnApplicationFocus(bool hasFocus)
-	{
-		ApplicationHasFocus = hasFocus;
-	}
+    internal bool GetBool(BoolPreference key)
+    {
+        return PlayerPrefs.GetInt(key.ToString()) != 0;
+    }
 
-	internal static AccountPreferences Get()
-	{
-		return s_instance;
-	}
+    private int GetInt(string key)
+    {
+        return PlayerPrefs.GetInt(key);
+    }
 
-	public static bool DoesApplicationHaveFocus()
-	{
-		int result;
-		if (s_instance != null)
-		{
-			result = (s_instance.ApplicationHasFocus ? 1 : 0);
-		}
-		else
-		{
-			result = 0;
-		}
-		return (byte)result != 0;
-	}
+    internal void SetBool(BoolPreference key, bool value)
+    {
+        PlayerPrefs.SetInt(key.ToString(), value ? 1 : 0);
+    }
 
-	private void Awake()
-	{
-		s_instance = this;
-	}
-
-	private void Start()
-	{
-		SetLanguageIndex();
-		for (int i = 0; i < m_defaultSettings[m_languageIndex].m_boolDefaults.Length; i++)
-		{
-			if (!PlayerPrefs.HasKey(m_defaultSettings[m_languageIndex].m_boolDefaults[i].m_preference.ToString()))
-			{
-				SetBool(m_defaultSettings[m_languageIndex].m_boolDefaults[i].m_preference, m_defaultSettings[m_languageIndex].m_boolDefaults[i].m_value);
-			}
-		}
-		while (true)
-		{
-			switch (5)
-			{
-			default:
-				return;
-			case 0:
-				break;
-			}
-		}
-	}
-
-	private void OnDestroy()
-	{
-		s_instance = null;
-	}
-
-	private void SetLanguageIndex()
-	{
-		m_languageIndex = 0;
-		int num = 0;
-		DefaultSettingsData[] defaultSettings = m_defaultSettings;
-		int num2 = 0;
-		while (true)
-		{
-			if (num2 < defaultSettings.Length)
-			{
-				DefaultSettingsData defaultSettingsData = defaultSettings[num2];
-				if (defaultSettingsData.Name == LocalizationManager.CurrentLanguageCode)
-				{
-					break;
-				}
-				num++;
-				num2++;
-				continue;
-			}
-			return;
-		}
-		while (true)
-		{
-			m_languageIndex = num;
-			return;
-		}
-	}
-
-	public int GetKeyDefaultsSize()
-	{
-		return m_defaultSettings[m_languageIndex].m_keyDefaults.Length;
-	}
-
-	public KeyCodeDefault GetKeyCodeDefault(int index)
-	{
-		return m_defaultSettings[m_languageIndex].m_keyDefaults[index];
-	}
-
-	internal bool GetBool(BoolPreference key)
-	{
-		return PlayerPrefs.GetInt(key.ToString()) != 0;
-	}
-
-	private int GetInt(string key)
-	{
-		return PlayerPrefs.GetInt(key);
-	}
-
-	internal void SetBool(BoolPreference key, bool value)
-	{
-		string key2 = key.ToString();
-		int value2;
-		if (value)
-		{
-			value2 = 1;
-		}
-		else
-		{
-			value2 = 0;
-		}
-		PlayerPrefs.SetInt(key2, value2);
-	}
-
-	private void SetInt(string key, int value)
-	{
-		PlayerPrefs.SetInt(key, value);
-	}
+    private void SetInt(string key, int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+    }
 }
