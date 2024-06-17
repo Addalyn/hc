d93faf30ef36daf9cf37638723f35864e72e552b
zheneq <zheneq@users.noreply.github.com>
2024-06-17 09:30:04 +0400

    Update KeyCodeData.cs

--- Assembly-CSharp/KeyCodeData.cs
+++ Assembly-CSharp/KeyCodeData.cs
@@ -3,15 +3,10 @@ using System;
 [Serializable]
 public class KeyCodeData
 {
-	public int m_primary;
-
-	public int m_modifierKey1;
-
-	public int m_additionalModifierKey1;
-
-	public int m_secondary;
-
-	public int m_modifierKey2;
-
-	public int m_additionalModifierKey2;
+    public int m_primary;
+    public int m_modifierKey1;
+    public int m_additionalModifierKey1;
+    public int m_secondary;
+    public int m_modifierKey2;
+    public int m_additionalModifierKey2;
 }
