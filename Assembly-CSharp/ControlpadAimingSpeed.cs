ad483876807213839ef6abe467b0760d7688f85d
zheneq <zheneq@users.noreply.github.com>
2024-06-17 16:20:24 +0400

    Update ControlpadAimingSpeed.cs

--- Assembly-CSharp/ControlpadAimingSpeed.cs
+++ Assembly-CSharp/ControlpadAimingSpeed.cs
@@ -2,57 +2,58 @@ using UnityEngine;
 
 public class ControlpadAimingSpeed
 {
-	public float m_startSpeed = 1f;
+    public float m_startSpeed = 1f;
+    public float m_maxSpeed = 1f;
+    public float m_acceleration;
+    public float m_minSpeed;
 
-	public float m_maxSpeed = 1f;
+    public float GetSpeed(float timeHeldDown, float multiplier = 1f)
+    {
+        float speed = (m_startSpeed + timeHeldDown * m_acceleration) * Mathf.Abs(multiplier);
+        return Mathf.Clamp(speed, m_minSpeed, m_maxSpeed);
+    }
 
-	public float m_acceleration;
+    public static ControlpadAimingSpeed DefaultAnalogStickDepth()
+    {
+        return new ControlpadAimingSpeed
+        {
+            m_startSpeed = 6f,
+            m_maxSpeed = 6f,
+            m_acceleration = 0f,
+            m_minSpeed = 0f
+        };
+    }
 
-	public float m_minSpeed;
+    public static ControlpadAimingSpeed DefaultDigitalButtonRotation()
+    {
+        return new ControlpadAimingSpeed
+        {
+            m_startSpeed = 2f,
+            m_maxSpeed = 360f,
+            m_acceleration = 180f,
+            m_minSpeed = 0f
+        };
+    }
 
-	public float GetSpeed(float timeHeldDown, float multiplier = 1f)
-	{
-		float value = (m_startSpeed + timeHeldDown * m_acceleration) * Mathf.Abs(multiplier);
-		return Mathf.Clamp(value, m_minSpeed, m_maxSpeed);
-	}
+    public static ControlpadAimingSpeed DefaultAnalogStickRotation()
+    {
+        return new ControlpadAimingSpeed
+        {
+            m_startSpeed = 20f,
+            m_maxSpeed = 360f,
+            m_acceleration = 720f,
+            m_minSpeed = 0f
+        };
+    }
 
-	public static ControlpadAimingSpeed DefaultAnalogStickDepth()
-	{
-		ControlpadAimingSpeed controlpadAimingSpeed = new ControlpadAimingSpeed();
-		controlpadAimingSpeed.m_startSpeed = 6f;
-		controlpadAimingSpeed.m_maxSpeed = 6f;
-		controlpadAimingSpeed.m_acceleration = 0f;
-		controlpadAimingSpeed.m_minSpeed = 0f;
-		return controlpadAimingSpeed;
-	}
-
-	public static ControlpadAimingSpeed DefaultDigitalButtonRotation()
-	{
-		ControlpadAimingSpeed controlpadAimingSpeed = new ControlpadAimingSpeed();
-		controlpadAimingSpeed.m_startSpeed = 2f;
-		controlpadAimingSpeed.m_maxSpeed = 360f;
-		controlpadAimingSpeed.m_acceleration = 180f;
-		controlpadAimingSpeed.m_minSpeed = 0f;
-		return controlpadAimingSpeed;
-	}
-
-	public static ControlpadAimingSpeed DefaultAnalogStickRotation()
-	{
-		ControlpadAimingSpeed controlpadAimingSpeed = new ControlpadAimingSpeed();
-		controlpadAimingSpeed.m_startSpeed = 20f;
-		controlpadAimingSpeed.m_maxSpeed = 360f;
-		controlpadAimingSpeed.m_acceleration = 720f;
-		controlpadAimingSpeed.m_minSpeed = 0f;
-		return controlpadAimingSpeed;
-	}
-
-	public static ControlpadAimingSpeed DefaultAnalogStickTranslation()
-	{
-		ControlpadAimingSpeed controlpadAimingSpeed = new ControlpadAimingSpeed();
-		controlpadAimingSpeed.m_startSpeed = 10f;
-		controlpadAimingSpeed.m_maxSpeed = 10f;
-		controlpadAimingSpeed.m_acceleration = 0f;
-		controlpadAimingSpeed.m_minSpeed = 0f;
-		return controlpadAimingSpeed;
-	}
+    public static ControlpadAimingSpeed DefaultAnalogStickTranslation()
+    {
+        return new ControlpadAimingSpeed
+        {
+            m_startSpeed = 10f,
+            m_maxSpeed = 10f,
+            m_acceleration = 0f,
+            m_minSpeed = 0f
+        };
+    }
 }
