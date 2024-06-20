c1cb76185735d45ce66ac460d32f9088f1f0befa
zheneq <zheneq@users.noreply.github.com>
2024-06-20 05:14:52 +0400

    Update GroupJoinManager.cs

--- Assembly-CSharp/GroupJoinManager.cs
+++ Assembly-CSharp/GroupJoinManager.cs
@@ -4,130 +4,91 @@ using System.Collections.Generic;
 
 internal class GroupJoinManager
 {
-	private static GroupJoinManager s_instance;
+    private static GroupJoinManager s_instance;
+    private Dictionary<string, GroupRequestWindow> m_pendingWindows = new Dictionary<string, GroupRequestWindow>();
+    private Dictionary<string, LeakyBucket> m_restrictSpammers = new Dictionary<string, LeakyBucket>();
 
-	private Dictionary<string, GroupRequestWindow> m_pendingWindows = new Dictionary<string, GroupRequestWindow>();
+    public static GroupJoinManager Get()
+    {
+        if (s_instance == null)
+        {
+            s_instance = new GroupJoinManager();
+        }
 
-	private Dictionary<string, LeakyBucket> m_restrictSpammers = new Dictionary<string, LeakyBucket>();
+        return s_instance;
+    }
 
-	public static GroupJoinManager Get()
-	{
-		if (s_instance == null)
-		{
-			s_instance = new GroupJoinManager();
-		}
-		return s_instance;
-	}
+    public void Update()
+    {
+        DateTime utcNow = DateTime.UtcNow;
+        List<GroupRequestWindow> expiredWindows = new List<GroupRequestWindow>();
+        foreach (GroupRequestWindow value in m_pendingWindows.Values)
+        {
+            if (value.HasExpired(utcNow))
+            {
+                expiredWindows.Add(value);
+            }
+        }
 
-	public void Update()
-	{
-		DateTime utcNow = DateTime.UtcNow;
-		List<GroupRequestWindow> list = new List<GroupRequestWindow>();
-		foreach (GroupRequestWindow value in m_pendingWindows.Values)
-		{
-			if (value.HasExpired(utcNow))
-			{
-				list.Add(value);
-			}
-		}
-		
-		list.ForEach(delegate(GroupRequestWindow p)
-			{
-				p.CleanupWindow();
-			});
-	}
+        expiredWindows.ForEach(delegate(GroupRequestWindow p) { p.CleanupWindow(); });
+    }
 
-	internal void AddRequest(GroupConfirmationRequest request)
-	{
-		if (UIFrontEnd.Get() == null)
-		{
-			while (true)
-			{
-				switch (5)
-				{
-				case 0:
-					break;
-				default:
-					return;
-				}
-			}
-		}
-		if (UIFrontEnd.Get().m_landingPageScreen != null)
-		{
-			if (UIFrontEnd.Get().m_landingPageScreen.m_inCustomGame)
-			{
-				SendGroupConfirmation(GroupInviteResponseType.PlayerInCustomMatch, request);
-				return;
-			}
-		}
-		if (m_pendingWindows.ContainsKey(request.LeaderFullHandle))
-		{
-			while (true)
-			{
-				switch (7)
-				{
-				case 0:
-					break;
-				default:
-					SendGroupConfirmation(GroupInviteResponseType.PlayerStillAwaitingPreviousQuery, request);
-					return;
-				}
-			}
-		}
-		if (request == null)
-		{
-			while (true)
-			{
-				switch (1)
-				{
-				case 0:
-					break;
-				default:
-					throw new Exception("request is null");
-				}
-			}
-		}
-		if (m_restrictSpammers.TryGetValue(request.LeaderFullHandle, out LeakyBucket value) && value != null)
-		{
-			if (!value.CanAdd())
-			{
-				while (true)
-				{
-					switch (1)
-					{
-					case 0:
-						break;
-					default:
-						SendGroupConfirmation(GroupInviteResponseType.RequestorSpamming, request);
-						return;
-					}
-				}
-			}
-		}
-		m_pendingWindows.Add(request.LeaderFullHandle, new GroupRequestWindow(request));
-	}
+    internal void AddRequest(GroupConfirmationRequest request)
+    {
+        if (UIFrontEnd.Get() == null)
+        {
+            return;
+        }
 
-	internal void RemoveRequest(GroupConfirmationRequest request)
-	{
-		m_pendingWindows.Remove(request.LeaderFullHandle);
-	}
+        if (UIFrontEnd.Get().m_landingPageScreen != null && UIFrontEnd.Get().m_landingPageScreen.m_inCustomGame)
+        {
+            SendGroupConfirmation(GroupInviteResponseType.PlayerInCustomMatch, request);
+        }
+        else if (m_pendingWindows.ContainsKey(request.LeaderFullHandle))
+        {
+            SendGroupConfirmation(GroupInviteResponseType.PlayerStillAwaitingPreviousQuery, request);
+        }
+        else if (request == null) // TODO CLIENT too late to check
+        {
+            throw new Exception("request is null");
+        }
+        else if (m_restrictSpammers.TryGetValue(request.LeaderFullHandle, out LeakyBucket spamBucket)
+                 && spamBucket != null
+                 && !spamBucket.CanAdd())
+        {
+            SendGroupConfirmation(GroupInviteResponseType.RequestorSpamming, request);
+        }
+        else
+        {
+            m_pendingWindows.Add(request.LeaderFullHandle, new GroupRequestWindow(request));
+        }
+    }
 
-	internal void SendGroupConfirmation(GroupInviteResponseType status, GroupConfirmationRequest request)
-	{
-		if (status == GroupInviteResponseType.PlayerRejected)
-		{
-			if (!m_restrictSpammers.TryGetValue(request.LeaderFullHandle, out LeakyBucket value))
-			{
-				value = new LeakyBucket(2.0, TimeSpan.FromMinutes(10.0));
-				m_restrictSpammers.Add(request.LeaderFullHandle, value);
-			}
-			value.Add();
-		}
-		GroupConfirmationResponse groupConfirmationResponse = new GroupConfirmationResponse();
-		groupConfirmationResponse.Acceptance = status;
-		groupConfirmationResponse.GroupId = request.GroupId;
-		groupConfirmationResponse.ConfirmationNumber = request.ConfirmationNumber;
-		groupConfirmationResponse.JoinerAccountId = request.JoinerAccountId;
-		ClientGameManager.Get().LobbyInterface.SendMessage(groupConfirmationResponse);
-	}
+    internal void RemoveRequest(GroupConfirmationRequest request)
+    {
+        m_pendingWindows.Remove(request.LeaderFullHandle);
+    }
+
+    internal void SendGroupConfirmation(GroupInviteResponseType status, GroupConfirmationRequest request)
+    {
+        if (status == GroupInviteResponseType.PlayerRejected)
+        {
+            if (!m_restrictSpammers.TryGetValue(request.LeaderFullHandle, out LeakyBucket spamBucket))
+            {
+                spamBucket = new LeakyBucket(2.0, TimeSpan.FromMinutes(10.0));
+                m_restrictSpammers.Add(request.LeaderFullHandle, spamBucket);
+            }
+
+            spamBucket.Add();
+        }
+
+        ClientGameManager.Get().LobbyInterface.SendMessage(
+            new GroupConfirmationResponse
+            {
+                Acceptance = status,
+                GroupId = request.GroupId,
+                ConfirmationNumber = request.ConfirmationNumber,
+                JoinerAccountId = request.JoinerAccountId
+            });
+    }
 }
