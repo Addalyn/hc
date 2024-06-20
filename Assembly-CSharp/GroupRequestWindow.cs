ef821b5349d8d7bc083c733715a3d02842053905
zheneq <zheneq@users.noreply.github.com>
2024-06-20 05:13:38 +0400

    Update GroupRequestWindow.cs

--- Assembly-CSharp/GroupRequestWindow.cs
+++ Assembly-CSharp/GroupRequestWindow.cs
@@ -3,129 +3,97 @@ using System;
 
 internal class GroupRequestWindow
 {
-	private GroupConfirmationRequest m_request;
+    private GroupConfirmationRequest m_request;
+    private UIPartyInvitePopDialogBox m_dialogBox;
+    private bool m_done;
+    private DateTime m_expiration;
 
-	private UIPartyInvitePopDialogBox m_dialogBox;
+    internal GroupRequestWindow(GroupConfirmationRequest request)
+    {
+        m_request = request;
+        m_expiration = DateTime.UtcNow + request.ExpirationTime;
+        SpawnNewPopup();
+    }
 
-	private bool m_done;
+    private void SpawnNewPopup()
+    {
+        switch (m_request.Type)
+        {
+            case GroupConfirmationRequest.JoinType.InviteToFormGroup:
+            {
+                m_dialogBox = UIDialogPopupManager.OpenPartyInviteDialog(
+                    StringUtil.TR("GroupRequest", "Global"),
+                    string.Format(StringUtil.TR("InviteToFormGroup", "Global"), m_request.LeaderName),
+                    StringUtil.TR("Join", "Global"),
+                    StringUtil.TR("Reject", "Global"),
+                    delegate { BlockPlayerFromGroupRequest(); },
+                    delegate { Join(); },
+                    delegate { Reject(); });
+                return;
+            }
+            case GroupConfirmationRequest.JoinType.RequestToJoinGroup:
+            {
+                m_dialogBox = UIDialogPopupManager.OpenPartyInviteDialog(
+                    StringUtil.TR("GroupRequest", "Global"),
+                    string.Format(StringUtil.TR("RequestToJoinGroup", "Invite"), m_request.JoinerName),
+                    StringUtil.TR("Approve", "Global"),
+                    StringUtil.TR("Reject", "Global"),
+                    delegate { BlockPlayerFromGroupRequest(); },
+                    delegate { Join(); },
+                    delegate { Reject(); });
+                break;
+            }
+        }
+    }
 
-	private DateTime m_expiration;
+    public void CleanupWindow()
+    {
+        if (m_done)
+        {
+            return;
+        }
 
-	internal GroupRequestWindow(GroupConfirmationRequest request)
-	{
-		m_request = request;
-		m_expiration = DateTime.UtcNow + request.ExpirationTime;
-		SpawnNewPopup();
-	}
+        MarkDone();
+        m_dialogBox.Close();
+        GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.OfferExpired, m_request);
+    }
 
-	private void SpawnNewPopup()
-	{
-		if (m_request.Type == GroupConfirmationRequest.JoinType._001D)
-		{
-			while (true)
-			{
-				switch (2)
-				{
-				case 0:
-					break;
-				default:
-				{
-					string description = string.Format(StringUtil.TR("InviteToFormGroup", "Global"), m_request.LeaderName);
-					m_dialogBox = UIDialogPopupManager.OpenPartyInviteDialog(StringUtil.TR("GroupRequest", "Global"), description, StringUtil.TR("Join", "Global"), StringUtil.TR("Reject", "Global"), delegate
-					{
-						BlockPlayerFromGroupRequest();
-					}, delegate
-					{
-						Join();
-					}, delegate
-					{
-						Reject();
-					});
-					return;
-				}
-				}
-			}
-		}
-		if (m_request.Type != GroupConfirmationRequest.JoinType._000E)
-		{
-			return;
-		}
-		while (true)
-		{
-			string description2 = string.Format(StringUtil.TR("RequestToJoinGroup", "Invite"), m_request.JoinerName);
-			m_dialogBox = UIDialogPopupManager.OpenPartyInviteDialog(StringUtil.TR("GroupRequest", "Global"), description2, StringUtil.TR("Approve", "Global"), StringUtil.TR("Reject", "Global"), delegate
-			{
-				BlockPlayerFromGroupRequest();
-			}, delegate
-			{
-				Join();
-			}, delegate
-			{
-				Reject();
-			});
-			return;
-		}
-	}
+    private void MarkDone()
+    {
+        m_done = true;
+        GroupJoinManager.Get().RemoveRequest(m_request);
+    }
 
-	public void CleanupWindow()
-	{
-		if (m_done)
-		{
-			return;
-		}
-		while (true)
-		{
-			MarkDone();
-			m_dialogBox.Close();
-			GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.OfferExpired, m_request);
-			return;
-		}
-	}
+    private void Join()
+    {
+        MarkDone();
+        GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.PlayerAccepted, m_request);
+    }
 
-	private void MarkDone()
-	{
-		m_done = true;
-		GroupJoinManager.Get().RemoveRequest(m_request);
-	}
+    private void Reject()
+    {
+        MarkDone();
+        GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.PlayerRejected, m_request);
+    }
 
-	private void Join()
-	{
-		MarkDone();
-		GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.PlayerAccepted, m_request);
-	}
+    private void BlockPlayerFromGroupRequest()
+    {
+        UIDialogPopupManager.OpenTwoButtonDialog(
+            StringUtil.TR("BlockPlayer", "FriendList"),
+            string.Format(StringUtil.TR("DoYouWantToBlock", "FriendList"), m_request.LeaderName),
+            StringUtil.TR("Yes", "Global"),
+            StringUtil.TR("No", "Global"),
+            delegate
+            {
+                MarkDone();
+                GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.PlayerRejected, m_request);
+                SlashCommands.Get().RunSlashCommand("/block", m_request.LeaderFullHandle);
+            },
+            delegate { SpawnNewPopup(); });
+    }
 
-	private void Reject()
-	{
-		MarkDone();
-		GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.PlayerRejected, m_request);
-	}
-
-	private void BlockPlayerFromGroupRequest()
-	{
-		string title = StringUtil.TR("BlockPlayer", "FriendList");
-		string description = string.Format(StringUtil.TR("DoYouWantToBlock", "FriendList"), m_request.LeaderName);
-		UIDialogPopupManager.OpenTwoButtonDialog(title, description, StringUtil.TR("Yes", "Global"), StringUtil.TR("No", "Global"), delegate
-		{
-			MarkDone();
-			GroupJoinManager.Get().SendGroupConfirmation(GroupInviteResponseType.PlayerRejected, m_request);
-			SlashCommands.Get().RunSlashCommand("/block", m_request.LeaderFullHandle);
-		}, delegate
-		{
-			SpawnNewPopup();
-		});
-	}
-
-	public bool HasExpired(DateTime time)
-	{
-		int result;
-		if (!m_done)
-		{
-			result = ((m_expiration < time) ? 1 : 0);
-		}
-		else
-		{
-			result = 0;
-		}
-		return (byte)result != 0;
-	}
+    public bool HasExpired(DateTime time)
+    {
+        return !m_done && m_expiration < time;
+    }
 }
--- Assembly-CSharp/LobbyGameClientMessages/GroupConfirmationRequest.cs
+++ Assembly-CSharp/LobbyGameClientMessages/GroupConfirmationRequest.cs
@@ -2,29 +2,22 @@ using System;
 
 namespace LobbyGameClientMessages
 {
-	[Serializable]
-	public class GroupConfirmationRequest : WebSocketMessage
-	{
-		public enum JoinType
-		{
-			_001D,
-			_000E
-		}
+    [Serializable]
+    public class GroupConfirmationRequest : WebSocketMessage
+    {
+        public enum JoinType
+        {
+            InviteToFormGroup,
+            RequestToJoinGroup
+        }
 
-		public long GroupId;
-
-		public string LeaderName;
-
-		public string LeaderFullHandle;
-
-		public string JoinerName;
-
-		public long JoinerAccountId;
-
-		public long ConfirmationNumber;
-
-		public TimeSpan ExpirationTime;
-
-		public JoinType Type;
-	}
+        public long GroupId;
+        public string LeaderName;
+        public string LeaderFullHandle;
+        public string JoinerName;
+        public long JoinerAccountId;
+        public long ConfirmationNumber;
+        public TimeSpan ExpirationTime;
+        public JoinType Type;
+    }
 }
