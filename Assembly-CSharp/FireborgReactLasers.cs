59d1656754f5b744206a9601cc71a4d9307c5883
zheneq <zheneq@users.noreply.github.com>
2024-07-01 00:48:38 +0400

    Server Fireborg Freelancer Stats remove unneeded

--- Assembly-CSharp/FireborgReactLasers.cs
+++ Assembly-CSharp/FireborgReactLasers.cs
@@ -367,14 +367,6 @@ public class FireborgReactLasers : GenericAbility_Container
         actorHitResults.Clear();
         actorHitResults.Add(casterHitResults);
     }
-	
-    // custom
-    public override void OnExecutedActorHit_Ability(ActorData caster, ActorData target, ActorHitResults results)
-    {
-        caster.GetFreelancerStats().AddToValueOfStat(
-            FreelancerStats.FireborgStats.BlastwaveDamage,
-            results.FinalDamage);
-    }
 
     // custom
     public override void OnExecutedActorHit_Effect(ActorData caster, ActorData target, ActorHitResults results)
