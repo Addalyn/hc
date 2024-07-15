4c5c4fcbe4d65cd5735397538e87d8cacb2b1203
zheneq <zheneq@users.noreply.github.com>
2024-07-15 15:28:33 +0400

    Update CharacterTypeExtensions.cs

--- Assembly-CSharp/CharacterTypeExtensions.cs
+++ Assembly-CSharp/CharacterTypeExtensions.cs
@@ -1,83 +1,45 @@
 public static class CharacterTypeExtensions
 {
-	public static string GetNameForCharacterType(CharacterType characterType, string displayName)
-	{
-		if (characterType != CharacterType.BattleMonk && characterType != CharacterType.BazookaGirl)
-		{
-			if (characterType != CharacterType.Blaster && characterType != CharacterType.Claymore)
-			{
-				if (characterType != CharacterType.DigitalSorceress)
-				{
-					if (characterType != CharacterType.Gremlins)
-					{
-						if (characterType != CharacterType.NanoSmith)
-						{
-							if (characterType != CharacterType.RageBeast)
-							{
-								if (characterType != CharacterType.RobotAnimal)
-								{
-									if (characterType != CharacterType.Scoundrel && characterType != CharacterType.Sniper)
-									{
-										if (characterType != CharacterType.SpaceMarine)
-										{
-											if (characterType != CharacterType.Spark && characterType != CharacterType.Tracker)
-											{
-												if (characterType != CharacterType.Trickster)
-												{
-													return characterType.ToString();
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		return displayName;
-	}
+    public static string GetNameForCharacterType(CharacterType characterType, string displayName)
+    {
+        return characterType != CharacterType.BattleMonk
+               && characterType != CharacterType.BazookaGirl
+               && characterType != CharacterType.Blaster
+               && characterType != CharacterType.Claymore
+               && characterType != CharacterType.DigitalSorceress
+               && characterType != CharacterType.Gremlins
+               && characterType != CharacterType.NanoSmith
+               && characterType != CharacterType.RageBeast
+               && characterType != CharacterType.RobotAnimal
+               && characterType != CharacterType.Scoundrel
+               && characterType != CharacterType.Sniper
+               && characterType != CharacterType.SpaceMarine
+               && characterType != CharacterType.Spark
+               && characterType != CharacterType.Tracker
+               && characterType != CharacterType.Trickster
+            ? characterType.ToString()
+            : displayName;
+    }
 
-	public static bool IsValidForHumanGameplay(this CharacterType characterType)
-	{
-		int result;
-		if (characterType > CharacterType.None)
-		{
-			if (!characterType.IsWillFill() && characterType != CharacterType.FemaleWillFill)
-			{
-				if (characterType != CharacterType.PunchingDummy)
-				{
-					result = ((characterType < CharacterType.Last) ? 1 : 0);
-					goto IL_0047;
-				}
-			}
-		}
-		result = 0;
-		goto IL_0047;
-		IL_0047:
-		return (byte)result != 0;
-	}
+    public static bool IsValidForHumanGameplay(this CharacterType characterType)
+    {
+        return characterType > CharacterType.None
+               && characterType < CharacterType.Last
+               && !characterType.IsWillFill()
+               && characterType != CharacterType.PunchingDummy
+               && characterType != CharacterType.FemaleWillFill;
+    }
 
-	public static bool IsValidForHumanPreGameSelection(this CharacterType characterType)
-	{
-		int result;
-		if (characterType > CharacterType.None && characterType != CharacterType.PunchingDummy)
-		{
-			if (characterType < CharacterType.Last)
-			{
-				result = ((characterType != CharacterType.FemaleWillFill) ? 1 : 0);
-				goto IL_0036;
-			}
-		}
-		result = 0;
-		goto IL_0036;
-		IL_0036:
-		return (byte)result != 0;
-	}
+    public static bool IsValidForHumanPreGameSelection(this CharacterType characterType)
+    {
+        return characterType > CharacterType.None
+               && characterType < CharacterType.Last
+               && characterType != CharacterType.PunchingDummy
+               && characterType != CharacterType.FemaleWillFill;
+    }
 
-	public static bool IsWillFill(this CharacterType characterType)
-	{
-		return characterType == CharacterType.PendingWillFill;
-	}
+    public static bool IsWillFill(this CharacterType characterType)
+    {
+        return characterType == CharacterType.PendingWillFill;
+    }
 }
