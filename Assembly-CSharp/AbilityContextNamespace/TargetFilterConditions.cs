de16066d653b033fd0e8e08efb692154825c11bf
zheneq <zheneq@users.noreply.github.com>
2024-07-30 15:40:40 +0400

    Update TargetFilterConditions.cs

--- Assembly-CSharp/AbilityContextNamespace/TargetFilterConditions.cs
+++ Assembly-CSharp/AbilityContextNamespace/TargetFilterConditions.cs
@@ -3,50 +3,52 @@ using System.Collections.Generic;
 
 namespace AbilityContextNamespace
 {
-	[Serializable]
-	public class TargetFilterConditions
-	{
-		public TeamFilter m_teamFilter;
-		public List<NumericContextValueCompareCond> m_numCompareConditions = new List<NumericContextValueCompareCond>();
+    [Serializable]
+    public class TargetFilterConditions
+    {
+        public TeamFilter m_teamFilter;
+        public List<NumericContextValueCompareCond> m_numCompareConditions = new List<NumericContextValueCompareCond>();
 
-		public TargetFilterConditions GetCopy()
-		{
-			TargetFilterConditions targetFilterConditions = MemberwiseClone() as TargetFilterConditions;
-			targetFilterConditions.m_numCompareConditions = new List<NumericContextValueCompareCond>();
-			for (int i = 0; i < m_numCompareConditions.Count; i++)
-			{
-				NumericContextValueCompareCond item = m_numCompareConditions[i].GetCopy();
-				targetFilterConditions.m_numCompareConditions.Add(item);
-			}
-			return targetFilterConditions;
-		}
+        public TargetFilterConditions GetCopy()
+        {
+            TargetFilterConditions targetFilterConditions = MemberwiseClone() as TargetFilterConditions;
+            // ReSharper disable once PossibleNullReferenceException
+            targetFilterConditions.m_numCompareConditions = new List<NumericContextValueCompareCond>();
+            foreach (NumericContextValueCompareCond cond in m_numCompareConditions)
+            {
+                targetFilterConditions.m_numCompareConditions.Add(cond.GetCopy());
+            }
 
-		public void AddTooltipTokens(List<TooltipTokenEntry> tokens, string prefix)
-		{
-		}
+            return targetFilterConditions;
+        }
 
-		public string GetInEditorDesc(string indent)
-		{
-			string text = indent + "Team = " + m_teamFilter.ToString() + "\n";
-			if (m_numCompareConditions != null)
-			{
-				foreach (NumericContextValueCompareCond numCompareCondition in m_numCompareConditions)
-				{
-					if (numCompareCondition.m_compareOp != ContextCompareOp.Ignore)
-					{
-						text = string.Concat(
-							text,
-							indent,
-							InEditorDescHelper.ContextVarName(numCompareCondition.m_contextName, !numCompareCondition.m_nonActorSpecificContext),
-							" is ",
-							numCompareCondition.m_compareOp,
-							" ",
-							InEditorDescHelper.ColoredString(numCompareCondition.m_testValue),
-							"\n");
-					}
-				}
-			}
-			return text;
-		}
-	}
+        public void AddTooltipTokens(List<TooltipTokenEntry> tokens, string prefix)
+        {
+        }
+
+        public string GetInEditorDesc(string indent)
+        {
+            string desc = indent + "Team = " + m_teamFilter + "\n";
+            if (m_numCompareConditions != null)
+            {
+                foreach (NumericContextValueCompareCond numCompareCondition in m_numCompareConditions)
+                {
+                    if (numCompareCondition.m_compareOp != ContextCompareOp.Ignore)
+                    {
+                        desc = string.Concat(
+                            desc,
+                            indent,
+                            InEditorDescHelper.ContextVarName(numCompareCondition.m_contextName, !numCompareCondition.m_nonActorSpecificContext),
+                            " is ",
+                            numCompareCondition.m_compareOp,
+                            " ",
+                            InEditorDescHelper.ColoredString(numCompareCondition.m_testValue),
+                            "\n");
+                    }
+                }
+            }
+
+            return desc;
+        }
+    }
 }
