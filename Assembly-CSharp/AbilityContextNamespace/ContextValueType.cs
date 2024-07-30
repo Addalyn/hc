99be1a8d33c4a8b4b7173be4c323f7caae752e02
zheneq <zheneq@users.noreply.github.com>
2024-07-30 14:53:49 +0400

    Update ContextValueType.cs

--- Assembly-CSharp/AbilityContextNamespace/ContextValueType.cs
+++ Assembly-CSharp/AbilityContextNamespace/ContextValueType.cs
@@ -1,10 +1,10 @@
 namespace AbilityContextNamespace
 {
-	public enum ContextValueType
-	{
-		// NOTE reintroduced enum value names
-		Int,
-		Float,
-		Vector3
-	}
+    public enum ContextValueType
+    {
+        // NOTE reintroduced enum value names
+        Int,
+        Float,
+        Vector3
+    }
 }
