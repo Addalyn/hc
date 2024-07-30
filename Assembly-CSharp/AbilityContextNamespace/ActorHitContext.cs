60dbb061d9dabac5a860a56e47e3bb29e56c61f2
zheneq <zheneq@users.noreply.github.com>
2024-07-30 14:47:05 +0400

    Rogues ActorHitContext

--- Assembly-CSharp/AbilityContextNamespace/ActorHitContext.cs
+++ Assembly-CSharp/AbilityContextNamespace/ActorHitContext.cs
@@ -1,3 +1,5 @@
+// ROGUES
+// SERVER
 using UnityEngine;
 
 namespace AbilityContextNamespace
@@ -7,7 +9,10 @@ namespace AbilityContextNamespace
         public Vector3 m_hitOrigin;
         public bool m_ignoreMinCoverDist;
         public bool m_inRangeForTargeter;
+        // public int m_ordinal; // rogues
         public ContextVars m_contextVars = new ContextVars();
+        // public List<int> m_targeterIndices = new List<int>(); // rogues
+        // public List<int> m_segmentIndices = new List<int>(); // rogues
 
         public void Reset()
         {
@@ -15,6 +20,35 @@ namespace AbilityContextNamespace
             m_contextVars.ClearData();
             m_inRangeForTargeter = false;
             m_ignoreMinCoverDist = false;
+            // m_ordinal = 0; // rogues
+            // m_targeterIndices.Clear(); // rogues
+            // m_segmentIndices.Clear(); // rogues
         }
+
+        // rogues?
+        // public ActorHitContext Clone()
+        // {
+        //     ActorHitContext actorHitContext = (ActorHitContext)MemberwiseClone();
+        //     actorHitContext.m_contextVars = m_contextVars.Clone();
+        //     return actorHitContext;
+        // }
+
+        // rogues
+        // public void AddTargeterIndex(int targeterIndex)
+        // {
+        //     if (!m_targeterIndices.Contains(targeterIndex))
+        //     {
+        //         m_targeterIndices.Add(targeterIndex);
+        //     }
+        // }
+
+        // rogues
+        // public void AddSegmentIndex(int segmentIndex)
+        // {
+        //     if (!m_segmentIndices.Contains(segmentIndex))
+        //     {
+        //         m_segmentIndices.Add(segmentIndex);
+        //     }
+        // }
     }
 }
