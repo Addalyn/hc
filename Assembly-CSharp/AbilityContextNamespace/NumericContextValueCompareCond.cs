7362cbcb7627882b3bc1969ab3e9b5842c935090
zheneq <zheneq@users.noreply.github.com>
2024-07-30 15:04:45 +0400

    Update NumericContextValueCompareCond.cs

--- Assembly-CSharp/AbilityContextNamespace/NumericContextValueCompareCond.cs
+++ Assembly-CSharp/AbilityContextNamespace/NumericContextValueCompareCond.cs
@@ -2,28 +2,29 @@ using System;
 
 namespace AbilityContextNamespace
 {
-	[Serializable]
-	public class NumericContextValueCompareCond
-	{
-		public string m_contextName;
-		public bool m_nonActorSpecificContext;
-		public ContextCompareOp m_compareOp;
-		public float m_testValue;
-		public bool m_ignoreIfNoContext;
-		private int m_contextKey;
+    [Serializable]
+    public class NumericContextValueCompareCond
+    {
+        public string m_contextName;
+        public bool m_nonActorSpecificContext;
+        public ContextCompareOp m_compareOp;
+        public float m_testValue;
+        public bool m_ignoreIfNoContext;
+        private int m_contextKey;
 
-		public int GetContextKey()
-		{
-			if (m_contextKey == 0)
-			{
-				m_contextKey = ContextVars.ToContextKey(m_contextName);
-			}
-			return m_contextKey;
-		}
+        public int GetContextKey()
+        {
+            if (m_contextKey == 0)
+            {
+                m_contextKey = ContextVars.ToContextKey(m_contextName);
+            }
 
-		public NumericContextValueCompareCond GetCopy()
-		{
-			return MemberwiseClone() as NumericContextValueCompareCond;
-		}
-	}
+            return m_contextKey;
+        }
+
+        public NumericContextValueCompareCond GetCopy()
+        {
+            return MemberwiseClone() as NumericContextValueCompareCond;
+        }
+    }
 }
