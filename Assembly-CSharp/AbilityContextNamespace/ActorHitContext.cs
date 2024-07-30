29abb700f30f6208842990036062ee5b88d42b7e
zheneq <zheneq@users.noreply.github.com>
2024-07-30 14:45:36 +0400

    Update ActorHitContext.cs

--- Assembly-CSharp/AbilityContextNamespace/ActorHitContext.cs
+++ Assembly-CSharp/AbilityContextNamespace/ActorHitContext.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 namespace AbilityContextNamespace
 {
-	public class ActorHitContext
-	{
-		public Vector3 m_hitOrigin;
-		public bool m_ignoreMinCoverDist;
-		public bool m_inRangeForTargeter;
-		public ContextVars m_contextVars = new ContextVars();
+    public class ActorHitContext
+    {
+        public Vector3 m_hitOrigin;
+        public bool m_ignoreMinCoverDist;
+        public bool m_inRangeForTargeter;
+        public ContextVars m_contextVars = new ContextVars();
 
-		public void Reset()
-		{
-			m_hitOrigin = Vector3.zero;
-			m_contextVars.ClearData();
-			m_inRangeForTargeter = false;
-			m_ignoreMinCoverDist = false;
-		}
-	}
+        public void Reset()
+        {
+            m_hitOrigin = Vector3.zero;
+            m_contextVars.ClearData();
+            m_inRangeForTargeter = false;
+            m_ignoreMinCoverDist = false;
+        }
+    }
 }
