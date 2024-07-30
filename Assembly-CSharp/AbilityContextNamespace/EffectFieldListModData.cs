c54bc9168accf156302444b033709faf17bc8eae
zheneq <zheneq@users.noreply.github.com>
2024-07-30 14:58:26 +0400

    Update EffectFieldListModData.cs

--- Assembly-CSharp/AbilityContextNamespace/EffectFieldListModData.cs
+++ Assembly-CSharp/AbilityContextNamespace/EffectFieldListModData.cs
@@ -3,50 +3,49 @@ using System.Collections.Generic;
 
 namespace AbilityContextNamespace
 {
-	[Serializable]
-	public class EffectFieldListModData
-	{
-		public List<OnHitEffecField> m_prependEffectFields;
-		public List<EffectFieldOverride> m_overrides;
+    [Serializable]
+    public class EffectFieldListModData
+    {
+        public List<OnHitEffecField> m_prependEffectFields;
+        public List<EffectFieldOverride> m_overrides;
 
-		public List<OnHitEffecField> GetModdedEffectFieldList(List<OnHitEffecField> input)
-		{
-			List<OnHitEffecField> list = new List<OnHitEffecField>();
-			for (int i = 0; i < m_prependEffectFields.Count; i++)
-			{
-				list.Add(m_prependEffectFields[i].GetCopy());
-			}
-			for (int j = 0; j < input.Count; j++)
-			{
-				string identifier = input[j].GetIdentifier();
-				OnHitEffecField onHitEffecField = this.GetOverrideEntry(identifier);
-				if (onHitEffecField != null)
-				{
-					list.Add(onHitEffecField.GetCopy());
-				}
-				else
-				{
-					list.Add(input[j].GetCopy());
-				}
-			}
-			return list;
-		}
+        public List<OnHitEffecField> GetModdedEffectFieldList(List<OnHitEffecField> input)
+        {
+            List<OnHitEffecField> list = new List<OnHitEffecField>();
+            foreach (OnHitEffecField effectField in m_prependEffectFields)
+            {
+                list.Add(effectField.GetCopy());
+            }
 
-		private OnHitEffecField GetOverrideEntry(string identifier)
-		{
-			if (string.IsNullOrEmpty(identifier))
-			{
-				return null;
-			}
-			for (int i = 0; i < m_overrides.Count; i++)
-			{
-				string text = m_overrides[i].GetIdentifier();
-				if (text.Equals(identifier, StringComparison.OrdinalIgnoreCase))
-				{
-					return m_overrides[i].m_effectOverride;
-				}
-			}
-			return null;
-		}
-	}
+            foreach (OnHitEffecField effectField in input)
+            {
+                OnHitEffecField overrideEntry = GetOverrideEntry(effectField.GetIdentifier());
+                list.Add(
+                    overrideEntry != null
+                        ? overrideEntry.GetCopy()
+                        : effectField.GetCopy());
+            }
+
+            return list;
+        }
+
+        private OnHitEffecField GetOverrideEntry(string identifier)
+        {
+            if (string.IsNullOrEmpty(identifier))
+            {
+                return null;
+            }
+
+            foreach (EffectFieldOverride effectFieldOverride in m_overrides)
+            {
+                string overrideIdentifier = effectFieldOverride.GetIdentifier();
+                if (overrideIdentifier.Equals(identifier, StringComparison.OrdinalIgnoreCase))
+                {
+                    return effectFieldOverride.m_effectOverride;
+                }
+            }
+
+            return null;
+        }
+    }
 }
