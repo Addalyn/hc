ee27af35092ee8192a5f5ea906dcd88fc6d11a66
zheneq <zheneq@users.noreply.github.com>
2024-07-31 14:31:40 +0400

    Build version for server & client

--- Assembly-CSharp/Properties/AssemblyInfo.cs
+++ Assembly-CSharp/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-using A;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -7,7 +6,34 @@ using System.Security.Permissions;
 
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
-[assembly: AssemblyInfo(ThisAssembly.Git.Tag)]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
-[assembly: AssemblyFileVersion(BuildVersion.s_buildTag)]
 [module: UnverifiableCode]
+
+[assembly: AssemblyVersion (
+    ThisAssembly.Git.BaseVersion.Major + "." +
+    ThisAssembly.Git.BaseVersion.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Patch + "." +
+    ThisAssembly.Git.Commits
+    )]
+
+[assembly: AssemblyFileVersion (
+    ThisAssembly.Git.BaseVersion.Major + "." +
+    ThisAssembly.Git.BaseVersion.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Patch + "." +
+    ThisAssembly.Git.Commits
+    )]
+
+[assembly: AssemblyInformationalVersion (
+    ThisAssembly.Git.BaseVersion.Major + "." + 
+    ThisAssembly.Git.BaseVersion.Minor + 
+    (ThisAssembly.Git.BaseVersion.Patch != "0" ? "." + ThisAssembly.Git.BaseVersion.Patch : "") +
+    (ThisAssembly.Git.SemVer.DashLabel != "-client" ? ThisAssembly.Git.SemVer.DashLabel : "")
+    )]
+
+[assembly: AssemblyProduct (
+#if SERVER
+    "Atlas Reactor Custom Server"
+#else
+    "Atlas Reactor Custom Client"
+#endif
+    )]
