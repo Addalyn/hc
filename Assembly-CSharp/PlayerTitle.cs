28ab29b7ec3ceb56c845fffbc0e1dd81c6e3948f
Addalyn <dev@addalyn.baby>
2024-08-03 19:47:48 +0200

    Cleanup not used

--- Assembly-CSharp/PlayerTitle.cs
+++ Assembly-CSharp/PlayerTitle.cs
@@ -6,11 +6,5 @@ public class PlayerTitle
     public int ID { get; set; }
     public string TitleText { get; set; }
     public string Handle { get; set; }
-
-    public string GetTitleText(int titleLevel)
-    {
-        // Adjust title text based on titleLevel if needed
-        return TitleText;
-    }
 }
 #endif
